3c625ce89074897102e58d5fff575b3047dff6be
Jaz-Wazz <inc.herobrine275@gmail.com>
2022-10-31 01:34:31 +0300

    Add simple http server.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,52 @@
+#include <boost/asio/co_spawn.hpp>
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/this_coro.hpp>
+#include <boost/beast/core/flat_buffer.hpp>
+#include <boost/beast/core/tcp_stream.hpp>
+#include <boost/beast/http/message.hpp>
+#include <boost/beast/http/read.hpp>
+#include <boost/beast/http/string_body.hpp>
+#include <boost/beast/http/write.hpp>
 #include <fmt/core.h>
+#include <iostream>
 #include <httpfox.hpp>
+
+namespace asio = boost::asio;					// NOLINT
+namespace beast = boost::beast;					// NOLINT
+namespace this_coro = boost::asio::this_coro;	// NOLINT
+
+asio::awaitable<void> session(beast::tcp_stream stream)
+{
+	// Get request.
+    beast::flat_buffer buf;
+	beast::http::request<beast::http::string_body> req;
+	co_await beast::http::async_read(stream, buf, req, asio::use_awaitable);
+	std::cout << req;
+
+	// Send response.
+	beast::http::response<beast::http::string_body> res;
+	res.body() = "hello, a ne idi naxui.";
+	co_await beast::http::async_write(stream, res, asio::use_awaitable);
+
+	// Disconnect.
+    stream.socket().shutdown(asio::ip::tcp::socket::shutdown_send);
+}
+
+asio::awaitable<void> listener()
+{
+	auto ctx = co_await this_coro::executor;
+	asio::ip::tcp::acceptor acceptor {ctx, {asio::ip::tcp::v4(), 80}};
+    for(;;)
+	{
+		beast::tcp_stream tcp_stream {co_await acceptor.async_accept(asio::use_awaitable)};
+		asio::co_spawn(ctx, session(std::move(tcp_stream)), hf::rethrowed);
+	}
+}
 
 int main()
 {
-	fmt::print("sas.\n");
-	return 0;
+    asio::io_context ctx;
+    asio::co_spawn(ctx, listener(), hf::rethrowed);
+    return ctx.run();
 }
