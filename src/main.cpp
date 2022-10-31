18ef70e057ad3420e6ea4c3265514b7c59f03fab
Jaz-Wazz <inc.herobrine275@gmail.com>
2022-10-31 08:14:32 +0300

    haxserver working prototype.

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include <boost/beast/http/empty_body.hpp>
 #include <boost/beast/http/message.hpp>
 #include <boost/beast/http/read.hpp>
+#include <boost/beast/http/status.hpp>
 #include <boost/beast/http/string_body.hpp>
 #include <boost/beast/http/verb.hpp>
 #include <boost/beast/http/write.hpp>
@@ -15,80 +16,81 @@
 #include <iostream>
 #include <httpfox.hpp>
 #include <stdexcept>
+#include <string>
 #include <utility>
+#include <expected>
 
 namespace asio = boost::asio;					// NOLINT
 namespace beast = boost::beast;					// NOLINT
 namespace this_coro = boost::asio::this_coro;	// NOLINT
 
-auto take_archive_data(std::string path) -> asio::awaitable<std::string>
+auto perform_http(auto host, auto & request, auto & response) -> asio::awaitable<void>
 {
-	auto perform_http = [](auto host, auto & request, auto & response) -> asio::awaitable<void>
-	{
-		auto ctx = co_await this_coro::executor;
-		asio::ip::tcp::resolver resolver {ctx};
-		auto resolver_results = co_await resolver.async_resolve(host, "http", asio::use_awaitable);
-		asio::ip::tcp::socket sock {ctx};
-		co_await asio::async_connect(sock, resolver_results, asio::use_awaitable);
-		request.set("Host", host);
-		co_await beast::http::async_write(sock, request, asio::use_awaitable);
-		beast::flat_buffer buffer;
-		co_await beast::http::async_read(sock, buffer, response, asio::use_awaitable);
-	};
+	auto ctx = co_await this_coro::executor;
+	asio::ip::tcp::resolver resolver {ctx};
+	auto resolver_results = co_await resolver.async_resolve(host, "http", asio::use_awaitable);
+	asio::ip::tcp::socket sock {ctx};
+	co_await asio::async_connect(sock, resolver_results, asio::use_awaitable);
+	request.set("Host", host);
+	co_await beast::http::async_write(sock, request, asio::use_awaitable);
+	beast::flat_buffer buffer;
+	co_await beast::http::async_read(sock, buffer, response, asio::use_awaitable);
+};
 
-	auto take_item_location = [&](std::string path) -> asio::awaitable<std::pair<std::string, std::string>>
-	{
-		beast::http::request<beast::http::empty_body> request {beast::http::verb::get, "/download" + path, 11};
-		beast::http::response<beast::http::string_body> response;
-		co_await perform_http("archive.org", request, response);
-		if(response.result_int() == 302)
-		{
-			auto location = response.at("Location");
-			co_return std::make_pair(location.substr(7, location.find('/', 7) - 7), location.substr(location.find('/', 7)));
-		}
-		else
-		{
-			// fmt::print("----------------------[EXCEPT DUMP]----------------------\n");
-			// std::cout << request << '\n' << response;
-			// fmt::print("---------------------------------------------------------\n");
-			throw std::runtime_error {fmt::format("Failed to get item location, code: {}", response.result_int())};
-		}
-	};
-
-	auto [host, original_path] = co_await take_item_location(path);
-	// fmt::print("Location: [{}] + [{}].\n", host, original_path);
-
-	beast::http::request<beast::http::empty_body> request {beast::http::verb::get, original_path, 11};
+auto take_archive_location(std::string path) -> asio::awaitable<std::expected<std::pair<std::string, std::string>, int>>
+{
+	beast::http::request<beast::http::empty_body> request {beast::http::verb::get, "/download" + path, 11};
 	beast::http::response<beast::http::string_body> response;
-	co_await perform_http(host, request, response);
-	co_return response.body();
+	co_await perform_http("archive.org", request, response);
+	if(response.result_int() == 302)
+	{
+		auto location = response.at("Location");
+		co_return std::make_pair(location.substr(7, location.find('/', 7) - 7), location.substr(location.find('/', 7)));
+	}
+	else co_return std::unexpected(response.result_int());
 }
 
-auto session(beast::tcp_stream stream) -> asio::awaitable<void> try
+auto http_get_contents(std::string host, std::string path) -> asio::awaitable<std::expected<std::string, int>>
 {
-	// Get request.
+	beast::http::request<beast::http::empty_body> request {beast::http::verb::get, path, 11};
+	beast::http::response<beast::http::string_body> response;
+	co_await perform_http(host, request, response);
+	if(response.result_int() == 200) co_return response.body(); else co_return std::unexpected(response.result_int());
+}
+
+auto session(beast::tcp_stream stream) -> asio::awaitable<void>
+{
     beast::flat_buffer buf;
 	beast::http::request<beast::http::string_body> req;
 	co_await beast::http::async_read(stream, buf, req, asio::use_awaitable);
-	// std::cout << req;
+	auto page = req.target().to_string();
 
-	// Send response.
-	beast::http::response<beast::http::string_body> res;
+	if(auto result = co_await take_archive_location(page))
+	{
+		auto [host, path] = result.value();
 
-	fmt::print("[>>>] - Request page: '{}'.\n", req.target().to_string());
-	auto data = co_await take_archive_data("/favicon.ico");
-	// res.body() = data;
-	// co_await beast::http::async_write(stream, res, asio::use_awaitable);
-	// fmt::print("[<<<] - Returned page: '{}'.\n", req.target().to_string());
+		if(auto result = co_await http_get_contents(host, path))
+		{
+			beast::http::response<beast::http::string_body> res;
+			res.body() = result.value();
+			co_await beast::http::async_write(stream, res, asio::use_awaitable);
+			fmt::print("[haxserv] - [{}] -> [{}{}] ({}).\n", page, host, path, result.value().size());
+		}
+		else
+		{
+			beast::http::response<beast::http::string_body> res {beast::http::status::not_found, 11};
+			co_await beast::http::async_write(stream, res, asio::use_awaitable);
+			fmt::print("[haxserv] - [{}] -> [404] (Get data error, code: '{}').\n", page, result.error());
+		}
+	}
+	else
+	{
+		beast::http::response<beast::http::string_body> res {beast::http::status::not_found, 11};
+		co_await beast::http::async_write(stream, res, asio::use_awaitable);
+		fmt::print("[haxserv] - [{}] -> [404] (Taking location error, code: '{}').\n", page, result.error());
+	}
 
-	// // Disconnect.
-    // stream.socket().shutdown(asio::ip::tcp::socket::shutdown_send);
-}
-catch(std::exception e)
-{
-	// std::string str = (*e.what() != '\n') ? e.what() : "unknown";
-	// fmt::print("[<<<] - Disconnect by except: '{}'.\n", e.what());
-	co_return;
+    stream.socket().shutdown(asio::ip::tcp::socket::shutdown_send);
 }
 
 auto listener() -> asio::awaitable<void>
