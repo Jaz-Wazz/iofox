8412af42e20dac9e1098cddabd63d8136a64bbe9
Jaz-Wazz <inc.herobrine275@gmail.com>
2022-10-31 03:18:57 +0300

    Archive http connect.

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,15 @@
+#include <boost/asio/awaitable.hpp>
 #include <boost/asio/co_spawn.hpp>
 #include <boost/asio/io_context.hpp>
 #include <boost/asio/ip/tcp.hpp>
 #include <boost/asio/this_coro.hpp>
 #include <boost/beast/core/flat_buffer.hpp>
 #include <boost/beast/core/tcp_stream.hpp>
+#include <boost/beast/http/empty_body.hpp>
 #include <boost/beast/http/message.hpp>
 #include <boost/beast/http/read.hpp>
 #include <boost/beast/http/string_body.hpp>
+#include <boost/beast/http/verb.hpp>
 #include <boost/beast/http/write.hpp>
 #include <fmt/core.h>
 #include <iostream>
@@ -44,9 +47,30 @@ asio::awaitable<void> listener()
 	}
 }
 
+asio::awaitable<void> test()
+{
+	auto ctx = co_await this_coro::executor;
+
+	boost::asio::ip::tcp::resolver resolver {ctx};
+	auto resolver_results = co_await resolver.async_resolve("archive.org", "http", asio::use_awaitable);
+
+	asio::ip::tcp::socket sock {ctx};
+	co_await asio::async_connect(sock, resolver_results, asio::use_awaitable);
+
+	beast::http::request<beast::http::empty_body> request {beast::http::verb::get, "/download/stream_archive/main.html", 11};
+	request.set("Host", "archive.org");
+	co_await beast::http::async_write(sock, request, asio::use_awaitable);
+	std::cout << request << '\n';
+
+	beast::flat_buffer buffer;
+	beast::http::response<beast::http::string_body> response;
+	co_await beast::http::async_read(sock, buffer, response, asio::use_awaitable);
+	std::cout << response << '\n';
+}
+
 int main()
 {
     asio::io_context ctx;
-    asio::co_spawn(ctx, listener(), hf::rethrowed);
+    asio::co_spawn(ctx, test(), hf::rethrowed);
     return ctx.run();
 }
