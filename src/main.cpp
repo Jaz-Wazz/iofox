f4f0e521752e5a19c9429f0d394ee312ff52b59f
Jaz-Wazz <inc.herobrine275@gmail.com>
2022-10-31 07:08:19 +0300

    Expected review.

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,22 @@
-#include <boost/asio/awaitable.hpp>
-#include <boost/asio/co_spawn.hpp>
-#include <boost/asio/detached.hpp>
-#include <boost/asio/io_context.hpp>
-#include <boost/asio/this_coro.hpp>
+#include <expected>
 #include <fmt/core.h>
-#include <iostream>
-#include <httpfox.hpp>
-#include <stdexcept>
-#include <utility>
 
-namespace asio = boost::asio;					// NOLINT
-namespace beast = boost::beast;					// NOLINT
-namespace this_coro = boost::asio::this_coro;	// NOLINT
-
-asio::awaitable<void> coro()
+std::expected<int, std::string> test() noexcept
 {
-	try
-	{
-		throw std::runtime_error {"x"};
-	}
-	catch(std::exception e) // [std::exception] -> crash | [std::exception &] -> work.
-	{
-		fmt::print("b.\n");
-		co_return;
-	}
-	catch(...)
-	{
-		fmt::print("a.\n");
-		co_return;
-	}
+	return 5;
+	return std::unexpected("ou may its not int");
 }
 
-int main() try
+int main()
 {
-    asio::io_context ctx;
-	asio::co_spawn(ctx, coro(), asio::detached);
-    return ctx.run();
-} catch(std::exception e) { fmt::print("Except: '{}'.\n", e.what()); }
+	if(auto i = test())
+	{
+		fmt::print("Int: '{}'.\n", i.value());
+	}
+	else
+	{
+		fmt::print("Error: '{}'.\n", i.error());
+	}
+
+	return 0;
+}
