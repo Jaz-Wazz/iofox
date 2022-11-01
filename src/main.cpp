015d991c07578678a52a6a4484862e848f11204e
Jaz-Wazz <inc.herobrine275@gmail.com>
2022-11-01 03:38:08 +0300

    Incremental experiments.

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,23 @@
 #include <boost/asio/awaitable.hpp>
+#include <boost/asio/buffer.hpp>
 #include <boost/asio/co_spawn.hpp>
 #include <boost/asio/io_context.hpp>
 #include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/steady_timer.hpp>
 #include <boost/asio/this_coro.hpp>
+#include <boost/asio/use_awaitable.hpp>
 #include <boost/beast/core/flat_buffer.hpp>
 #include <boost/beast/core/tcp_stream.hpp>
 #include <boost/beast/http/empty_body.hpp>
+#include <boost/beast/http/impl/file_body_win32.hpp>
 #include <boost/beast/http/message.hpp>
 #include <boost/beast/http/read.hpp>
+#include <boost/beast/http/serializer.hpp>
 #include <boost/beast/http/status.hpp>
 #include <boost/beast/http/string_body.hpp>
 #include <boost/beast/http/verb.hpp>
 #include <boost/beast/http/write.hpp>
+#include <chrono>
 #include <fmt/core.h>
 #include <iostream>
 #include <httpfox.hpp>
@@ -65,30 +71,110 @@ auto session(beast::tcp_stream stream) -> asio::awaitable<void>
 	co_await beast::http::async_read(stream, buf, req, asio::use_awaitable);
 	auto page = req.target().to_string();
 
-	if(auto result = co_await take_archive_location(page))
-	{
-		auto [host, path] = result.value();
+	// if(auto result = co_await take_archive_location(page))
+	// {
+	// 	auto [host, path] = result.value();
 
-		if(auto result = co_await http_get_contents(host, path))
-		{
-			beast::http::response<beast::http::string_body> res;
-			res.body() = result.value();
-			co_await beast::http::async_write(stream, res, asio::use_awaitable);
-			fmt::print("[haxserv] - [{}] -> [{}{}] ({}).\n", page, host, path, result.value().size());
-		}
-		else
-		{
-			beast::http::response<beast::http::string_body> res {beast::http::status::not_found, 11};
-			co_await beast::http::async_write(stream, res, asio::use_awaitable);
-			fmt::print("[haxserv] - [{}] -> [404] (Get data error, code: '{}').\n", page, result.error());
-		}
-	}
-	else
-	{
-		beast::http::response<beast::http::string_body> res {beast::http::status::not_found, 11};
-		co_await beast::http::async_write(stream, res, asio::use_awaitable);
-		fmt::print("[haxserv] - [{}] -> [404] (Taking location error, code: '{}').\n", page, result.error());
-	}
+	// 	if(auto result = co_await http_get_contents(host, path))
+	// 	{
+			// beast::http::response<beast::http::string_body> res;
+			// res.body() = result.value();
+			// co_await beast::http::async_write(stream, res, asio::use_awaitable);
+			// fmt::print("[haxserv] - [{}] -> [{}{}] ({}).\n", page, host, path, result.value().size());
+
+			// beast::http::response<beast::http::string_body> res;
+			// beast::http::response_serializer<beast::http::string_body> serializer {};
+			// co_await beast::http::async_write_header(stream, serializer, asio::use_awaitable);
+
+			// stream.async_write_some()
+
+			// while(1)
+			// {
+			// 	co_await asio::steady_timer(co_await this_coro::executor, std::chrono::seconds(1)).async_wait(asio::use_awaitable);
+			// 	co_await stream.async_write_some(asio::buffer("sas"), asio::use_awaitable);
+			// }
+
+			for(int i = 0; i < 10; i++)
+			{
+				co_await asio::steady_timer(co_await this_coro::executor, std::chrono::seconds(1)).async_wait(asio::use_awaitable);
+				res.body() += "hui";
+				serializer.consume(3);
+				co_await beast::http::async_write_some(stream, serializer, asio::use_awaitable);
+			}
+
+
+
+			// serializer.
+			// co_await beast::http::async_write_some(stream, serializer, asio::use_awaitable);
+
+			// // Data.
+			// auto host = "ia902302.us.archive.org";
+			// auto path = "/22/items/1_20211016_20211016/1.png";
+
+			// // Resolve.
+			// auto ctx = co_await this_coro::executor;
+			// asio::ip::tcp::resolver resolver {ctx};
+			// auto resolver_results = co_await resolver.async_resolve(host, "http", asio::use_awaitable);
+
+			// // Connect.
+			// asio::ip::tcp::socket sock {ctx};
+			// co_await asio::async_connect(sock, resolver_results, asio::use_awaitable);
+
+			// // Send request to archive.
+			// beast::http::request<beast::http::empty_body> request {beast::http::verb::get, path, 11};
+			// request.set("Host", host);
+			// co_await beast::http::async_write(sock, request, asio::use_awaitable);
+
+			// // Read.
+			// beast::http::response_parser<beast::http::string_body> parser;
+			// beast::flat_buffer buf;
+
+			// // Read header.
+			// co_await beast::http::async_read_header(sock, buf, parser, asio::use_awaitable);
+			// std::cout << parser.get() << '\n';
+
+			// // If normal connect.
+			// if(parser.get().result_int() == 200)
+			// {
+			// 	// Send header to client.
+			// 	beast::http::response<beast::http::string_body> res;
+			// 	beast::http::response_serializer<beast::http::string_body> serializer {res};
+			// 	co_await beast::http::async_write_header(sock, serializer, asio::use_awaitable);
+			// 	std::cout << res;
+
+			// 	// Incremental read body.
+			// 	while(!parser.is_done())
+			// 	{
+			// 		auto bytes_count = co_await beast::http::async_read_some(sock, buf, parser, asio::use_awaitable);
+			// 		char * bytes = static_cast<char *>(buf.data().data());
+			// 		// std::cout.write(bytes, bytes_count);
+			// 		// fmt::print("Readed: '{}'.\n", bytes_count);
+
+			// 		// co_await beast::http::async_write_some(sock, serializer, asio::use_awaitable);
+			// 		serializer.get()
+			// 		co_await beast::http::async_write_some(sock, serializer, asio::use_awaitable);
+			// 	}
+			// }
+			// else
+			// {
+			// 	beast::http::response<beast::http::string_body> res {beast::http::status::not_found, 11};
+			// 	co_await beast::http::async_write(stream, res, asio::use_awaitable);
+			// 	fmt::print("[haxserv] - Error 1.\n");
+			// }
+	// 	}
+	// 	else
+	// 	{
+	// 		beast::http::response<beast::http::string_body> res {beast::http::status::not_found, 11};
+	// 		co_await beast::http::async_write(stream, res, asio::use_awaitable);
+	// 		fmt::print("[haxserv] - [{}] -> [404] (Get data error, code: '{}').\n", page, result.error());
+	// 	}
+	// }
+	// else
+	// {
+	// 	beast::http::response<beast::http::string_body> res {beast::http::status::not_found, 11};
+	// 	co_await beast::http::async_write(stream, res, asio::use_awaitable);
+	// 	fmt::print("[haxserv] - [{}] -> [404] (Taking location error, code: '{}').\n", page, result.error());
+	// }
 
     stream.socket().shutdown(asio::ip::tcp::socket::shutdown_send);
 }
