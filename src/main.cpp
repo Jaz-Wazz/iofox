5d0495b3f87e66c7ce5a10ed6779969e9234a4eb
Jaz-Wazz <inc.herobrine275@gmail.com>
2022-11-11 14:40:49 +0300

    context_local pre release.

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,50 @@
+#include <boost/asio/any_io_executor.hpp>
 #include <boost/asio/awaitable.hpp>
 #include <boost/asio/co_spawn.hpp>
+#include <boost/asio/execution_context.hpp>
 #include <boost/asio/io_context.hpp>
+#include <boost/asio/this_coro.hpp>
+#include <exception>
 #include <fmt/core.h>
 #include <httpfox.hpp>
 #include <fastdevhax.hpp>
 #include <jaja_notation.hpp>
+#include <optional>
 
 template <typename T> class context_local
 {
-	prv class service_cls: public asio::execution_context::service
+	prv class service: public asio::execution_context::service, public std::optional<T>
 	{
 		pbl using key_type = service;
 		pbl using id = service;
-		pbl T var = 0;
-		pbl service_cls(asio::execution_context & context): asio::execution_context::service(context) {}
+		pbl service(asio::execution_context & ctx): asio::execution_context::service(ctx) {}
 		pbl void shutdown() {}
-		pbl ~service() {}
 	};
 
-	pbl auto value() -> asio::awaitable<std::reference_wrapper<T>>
+	pbl auto value_or_emplace(asio::any_io_executor && executor, auto... args) -> T &
 	{
-		auto & execution_context = (co_await this_coro::executor).context();
-		if(!asio::has_service<service_cls>(execution_context)) asio::make_service<service_cls>(execution_context);
-		co_return asio::use_service<service_cls>(execution_context).var;
+		if(!asio::has_service<service>(executor.context())) asio::make_service<service>(executor.context());
+		service & s = asio::use_service<service>(executor.context());
+		if(!s) s.emplace(std::forward<decltype(args)>(args)...);
+		return s.value();
 	}
 };
 
-context_local<int> var;
+class res
+{
+	pbl res() { fmt::print("construct.\n"); }
+	pbl ~res() { fmt::print("destruct.\n"); }
+	pbl int i = 10;
+};
 
 auto coro(char id) -> asio::awaitable<void>
 {
-	int & x = co_await var.value();
-	fmt::print("[coro '{}'] - Value: '{}'.\n", id, x++);
+	static context_local<res> var;
+	auto & x = var.value_or_emplace(co_await this_coro::executor);
+	fmt::print("[coro '{}'] - Value: '{}'.\n", id, x.i++);
 }
 
-int main()
+int main() try
 {
 	// First ctx.
 	{
@@ -50,4 +60,4 @@ int main()
 		ctx.run();
 	}
 	return 0;
-}
+} catch(std::exception & e) { fmt::print("Except: '{}'.\n", e.what()); }
