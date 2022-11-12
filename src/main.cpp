fa305c2563931d807487ebae0533fdf9aa7b34e4
Jaz-Wazz <inc.herobrine275@gmail.com>
2022-11-12 22:42:10 +0300

    netfox alpha.

--- src/main.cpp
+++ src/main.cpp
@@ -1,79 +1,24 @@
-#include <boost/asio/any_io_executor.hpp>
 #include <boost/asio/awaitable.hpp>
 #include <boost/asio/co_spawn.hpp>
-#include <boost/asio/execution_context.hpp>
+#include <boost/asio/detached.hpp>
 #include <boost/asio/io_context.hpp>
-#include <boost/asio/ip/tcp.hpp>
-#include <boost/asio/this_coro.hpp>
-#include <boost/asio/use_awaitable.hpp>
 #include <exception>
 #include <fmt/core.h>
+#include <netfox.hpp>
 #include <httpfox.hpp>
-#include <fastdevhax.hpp>
-#include <jaja_notation.hpp>
-#include <optional>
-#include <boost/core/noncopyable.hpp>
 
-namespace netfox::system
+namespace asio = boost::asio; // NOLINT
+
+auto coro() -> asio::awaitable<void>
 {
-	template <typename T> class context_local: boost::noncopyable
-	{
-		pbl explicit context_local() {}
-
-		prv class service: public asio::execution_context::service, public std::optional<T>
-		{
-			pbl using key_type = service;
-			pbl using id = service;
-			pbl service(asio::execution_context & ctx): asio::execution_context::service(ctx) {}
-			pbl void shutdown() {}
-		};
-
-		pbl auto value_or_emplace(asio::any_io_executor && executor, auto... args) -> T &
-		{
-			if(!asio::has_service<service>(executor.context())) asio::make_service<service>(executor.context());
-			service & s = asio::use_service<service>(executor.context());
-			if(!s) s.emplace(std::forward<decltype(args)>(args)...);
-			return s.value();
-		}
-    };
-}
-
-namespace netfox::dns
-{
-	auto resolve(auto protocol, auto host) -> asio::awaitable<asio::ip::tcp::resolver::results_type>
-	{
-		static netfox::system::context_local<asio::ip::tcp::resolver> resolver;
-		auto & r = resolver.value_or_emplace(co_await this_coro::executor, co_await this_coro::executor);
-		co_return co_await r.async_resolve(host, protocol, asio::use_awaitable);
-	}
-}
-
-auto coro(char id) -> asio::awaitable<void>
-{
-	// netfox::dns::interrupt();
-	// netfox::ssl::context()
-
-	auto result = co_await netfox::dns::resolve("https", "google.com");
-	for(auto el : result) fmt::print("[{}] - Ip: '{}'.\n", id, el.endpoint().address().to_string());
-
-	// static netfox::system::context_local<int> var;
-	// fmt::print("[coro '{}'] - Value: '{}'.\n", id, var.value_or_emplace(co_await this_coro::executor, 25)++);
+	auto ret = co_await netfox::dns::resolve("http", "exmaple.com");
+	for(auto el : ret) fmt::print("Ip: '{}'.\n", el.endpoint().address().to_string());
+	co_return;
 }
 
 int main() try
 {
-	// First ctx.
-	{
-		asio::io_context ctx;
-		for(int i = 0; i < 2; i++) asio::co_spawn(ctx, coro('a'), hf::rethrowed);
-		ctx.run();
-	}
-
-	// Second ctx.
-	{
-		asio::io_context ctx;
-		for(int i = 0; i < 2; i++) asio::co_spawn(ctx, coro('b'), hf::rethrowed);
-		ctx.run();
-	}
-	return 0;
+	asio::io_context ctx;
+	asio::co_spawn(ctx, coro(), asio::detached);
+	return ctx.run();
 } catch(std::exception & e) { fmt::print("Except: '{}'.\n", e.what()); }
--- src/netfox.hpp
+++ src/netfox.hpp
@@ -1,98 +1,53 @@
 #pragma once
-#include <boost/asio/as_tuple.hpp>
+#include <boost/asio/any_io_executor.hpp>
 #include <boost/asio/awaitable.hpp>
-#include <boost/asio/connect.hpp>
+#include <boost/asio/execution_context.hpp>
 #include <boost/asio/ip/tcp.hpp>
-#include <boost/asio/ssl/stream.hpp>
 #include <boost/asio/this_coro.hpp>
 #include <boost/asio/use_awaitable.hpp>
-#include <boost/beast/core/flat_buffer.hpp>
-#include <boost/beast/http/buffer_body.hpp>
-#include <boost/beast/http/error.hpp>
-#include <boost/beast/http/parser.hpp>
-#include <boost/beast/http/read.hpp>
-#include <boost/beast/http/serializer.hpp>
-#include <boost/beast/http/write.hpp>
-#include <expected>
-#include <jaja_notation.hpp>
+#include <boost/core/noncopyable.hpp>
 #include <optional>
 
-namespace netfox
+#define asio		boost::asio
+#define this_coro	asio::this_coro
+#define prv			private:
+#define pbl			public:
+
+namespace netfox::system
 {
-	enum class side {client, server};
+	template <typename T> class context_local: boost::noncopyable
+	{
+		pbl explicit context_local() {}
+
+		prv class service: public asio::execution_context::service, public std::optional<T>
+		{
+			pbl using key_type = service;
+			pbl using id = service;
+			pbl service(asio::execution_context & ctx): asio::execution_context::service(ctx) {}
+			pbl void shutdown() {}
+		};
+
+		pbl auto value_or_emplace(asio::any_io_executor && executor, auto... args) -> T &
+		{
+			if(!asio::has_service<service>(executor.context())) asio::make_service<service>(executor.context());
+			service & s = asio::use_service<service>(executor.context());
+			if(!s) s.emplace(std::forward<decltype(args)>(args)...);
+			return s.value();
+		}
+    };
 }
 
-namespace netfox::detail
+namespace netfox::dns
 {
-	namespace asio		= boost::asio;				// NOLINT
-	namespace this_coro = boost::asio::this_coro;	// NOLINT
-	namespace beast		= boost::beast;				// NOLINT
-	namespace http		= boost::beast::http;		// NOLINT
-
-	template <side> class http_stream;
-
-	template <> class http_stream<side::client>
+	auto resolve(auto protocol, auto host) -> asio::awaitable<asio::ip::tcp::resolver::results_type>
 	{
-		prv std::optional<asio::ip::tcp::socket> stream;
-		prv http::response_parser<http::buffer_body> parser;
-		prv std::optional<http::request<http::buffer_body>> serializer_request;
-		prv std::optional<http::request_serializer<http::buffer_body>> serializer;
-		prv beast::flat_buffer buf;
-
-		pbl auto connect(auto host) -> asio::awaitable<void>
-		{
-			auto ctx = co_await this_coro::executor;
-			stream.emplace(ctx);
-			auto resolver_results = co_await asio::ip::tcp::resolver(ctx).async_resolve(host, "http", asio::use_awaitable);
-			co_await asio::async_connect(stream->lowest_layer(), resolver_results, asio::use_awaitable);
-		}
-
-		pbl auto write_request(auto & request) noexcept -> asio::awaitable<std::expected<void, int>>
-		{
-			co_await http::async_write(*stream, request, asio::use_awaitable);
-		}
-
-		pbl auto write_request_header(auto & header) -> asio::awaitable<void>
-		{
-			serializer_request.emplace(header);
-			serializer.emplace(*serializer_request);
-			co_await http::async_write_header(*stream, *serializer, asio::use_awaitable);
-		}
-
-		pbl auto write_request_body();
-		pbl auto write_request_body_chunk();
-
-		pbl auto read_response(auto & response) -> asio::awaitable<void>
-		{
-			co_await http::async_read(*stream, buf, response, asio::use_awaitable);
-		}
-
-		pbl auto read_response_header(auto & header) -> asio::awaitable<void>
-		{
-			co_await http::async_read_header(*stream, buf, parser, asio::use_awaitable);
-			header = parser.get().base();
-		}
-
-		pbl auto read_response_body();
-
-		pbl auto read_response_body_chunk(char * buffer, std::size_t size) -> asio::awaitable<std::optional<std::size_t>>
-		{
-			if(!parser.is_done())
-			{
-				parser.get().body().data = buffer;
-				parser.get().body().size = size;
-				auto [err, bytes_readed] = co_await http::async_read(*stream, buf, parser, asio::as_tuple(asio::use_awaitable));
-				if(err.failed() && err != http::error::need_buffer) throw err;
-				co_return size - parser.get().body().size;
-			} else co_return std::nullopt;
-		}
-	};
-}
-
-namespace netfox
-{
-	namespace http
-	{
-		template <side side> using stream = detail::http_stream<side>;
+		static netfox::system::context_local<asio::ip::tcp::resolver> resolver;
+		auto & r = resolver.value_or_emplace(co_await this_coro::executor, co_await this_coro::executor);
+		co_return co_await r.async_resolve(host, protocol, asio::use_awaitable);
 	}
 }
+
+#undef asio
+#undef this_coro
+#undef prv
+#undef pbl
