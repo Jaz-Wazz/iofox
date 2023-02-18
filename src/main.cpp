f4b7dd3b0ba735e6ea5acadca94b05a5c4f7b686
Jaz-Wazz <inc.herobrine275@gmail.com>
2023-02-18 05:37:46 +0300

    Basic http experiments + Separated asio compile.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,13 @@
+#include <boost/asio/buffer.hpp>
+#include <boost/asio/co_spawn.hpp>
+#include <boost/asio/connect.hpp>
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/read.hpp>
+#include <boost/asio/read_until.hpp>
+#include <boost/asio/streambuf.hpp>
+#include <boost/asio/this_coro.hpp>
+#include <boost/asio/write.hpp>
 #include <boost/buffers/const_buffer.hpp>
 #include <boost/http_proto/request_parser.hpp>
 #include <boost/http_proto/request.hpp>
@@ -5,33 +15,56 @@
 #include <boost/http_proto/serializer.hpp>
 #include <boost/http_proto/status.hpp>
 #include <boost/http_proto/string_body.hpp>
+#include <boost/http_proto/version.hpp>
 #include <exception>
 #include <fmt/core.h>
 #include <iostream>
 #include <string>
 #include <vector>
+#include <iofox.hpp>
 
-namespace http_proto = boost::http_proto; // NOLINT.
+namespace http_proto = boost::http_proto;	// NOLINT.
+namespace asio = boost::asio;				// NOLINT.
+namespace this_coro = asio::this_coro; 		// NOLINT.
 
-auto serialize(http_proto::response response) -> std::string
+auto coro() -> io::coro<void>
 {
-	std::string ret;
-	http_proto::serializer serializer;
-	serializer.start(response);
-	for(auto buffer : serializer.prepare().value()) ret.append(static_cast<const char *>(buffer.data()), buffer.size());
-	return ret;
+	asio::ip::tcp::socket sock {co_await this_coro::executor};
+	co_await asio::async_connect(sock, co_await io::dns::resolve("http", "exmaple.com"), io::use_coro);
+	fmt::print("connected.\n");
+
+	http_proto::request request;
+	request.set_start_line("GET", "/", http_proto::version::http_1_1);
+	request.set("Host", "exmaple.com");
+	request.set("Connection", "close");
+	fmt::print("request:\n{}\n", request.buffer());
+
+	auto ret = co_await asio::async_write(sock, asio::buffer(request.buffer()), io::use_coro);
+	fmt::print("writed: '{}'.\n", ret);
+
+	// std::string buf;
+	// buf.resize(100);
+	// co_await asio::async_read(sock, asio::buffer(buf), io::use_coro);
+	// fmt::print("readed:\n{}\n", buf);
+
+	// std::string buf;
+	// co_await asio::async_read(sock, asio::dynamic_buffer(buf), io::use_coro);
+	// fmt::print("readed:\n{}\n", buf);
+
+	std::string str;
+	co_await asio::async_read_until(sock, asio::dynamic_buffer(str), "\r\n", io::use_coro);
+	fmt::print("readed:\n{}\n", str);
+
+	std::string str2;
+	co_await asio::async_read_until(sock, asio::dynamic_buffer(str2), "\r\n", io::use_coro_tuple);
+	fmt::print("readed:\n{}\n", str2);
 }
 
 int main() try
 {
-	http_proto::response response;
-	response.set_start_line(http_proto::status::ok);
-	response.set("Tails", "9");
-	response.set("Paws", "4");
-
-	fmt::print("Response serialized:\n{}\n", serialize(response));
-	fmt::print("Response buffer:\n{}\n", response.buffer());
-	return 0;
+	asio::io_context ctx;
+	asio::co_spawn(ctx, coro(), io::rethrowed);
+	return ctx.run();
 }
 catch(const std::exception & e)
 {
--- src/prebuild.cpp
+++ src/prebuild.cpp
@@ -1,2 +1,4 @@
 #include <boost/buffers/src.hpp>
 #include <boost/http_proto/src.hpp>
+#include <boost/asio/impl/src.hpp>
+#include <boost/asio/ssl/impl/src.hpp>
