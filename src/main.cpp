60d609caa45d8ac335aa09d79454b5f699372147
Jaz-Wazz <inc.herobrine275@gmail.com>
2023-02-22 03:22:21 +0300

    Some strange parser experiments.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <array>
 #include <boost/asio/buffer.hpp>
 #include <boost/asio/co_spawn.hpp>
 #include <boost/asio/connect.hpp>
@@ -8,10 +9,20 @@
 #include <boost/asio/streambuf.hpp>
 #include <boost/asio/this_coro.hpp>
 #include <boost/asio/write.hpp>
+#include <boost/buffers/buffer.hpp>
+#include <boost/buffers/circular_buffer.hpp>
 #include <boost/buffers/const_buffer.hpp>
+#include <boost/buffers/flat_buffer.hpp>
+#include <boost/buffers/mutable_buffer.hpp>
+#include <boost/buffers/sink.hpp>
+#include <boost/http_proto/detail/header.hpp>
+#include <boost/http_proto/error_types.hpp>
+#include <boost/http_proto/message_base.hpp>
+#include <boost/http_proto/parser.hpp>
 #include <boost/http_proto/request_parser.hpp>
 #include <boost/http_proto/request.hpp>
 #include <boost/http_proto/response.hpp>
+#include <boost/http_proto/response_parser.hpp>
 #include <boost/http_proto/serializer.hpp>
 #include <boost/http_proto/status.hpp>
 #include <boost/http_proto/string_body.hpp>
@@ -41,21 +52,75 @@ void hexdump(const void * ptr, std::size_t size)
 	}
 }
 
+#include <boost/buffers.hpp>
+#include <boost/http_proto.hpp>
+
+std::string data
+{
+	"HTTP/1.1 200 OK\r\n"
+	"cache-control: private\r\n"
+	"content-type: text/html; charset=utf-8\r\n"
+	"strict-transport-security: max-age=15552000\r\n"
+	"x-frame-options: SAMEORIGIN\r\n"
+	"content-length: 8\r\n"
+	"\r\n"
+	"somebody"
+};
+
+void test_inherit_parser()
+{
+	http_proto::response response {data};
+	hexdump(response.buffer().data(), response.buffer().size());
+};
+
+void test_inherit_parser_base()
+{
+	// http_proto::message_base response {http_proto::detail::kind::response, data};
+	// hexdump(response.buffer().data(), response.buffer().size());
+};
+
+void some_tests()
+{
+	http_proto::response_parser parser;
+	parser.start();
+
+	for(auto buffer : parser.prepare())
+	{
+		fmt::print("[write buffer] - data: '{}', size: '{}'.\n", buffer.data(), buffer.size());
+		if(buffer.data() != nullptr && buffer.size() != 0)
+		{
+			for(int i = 0; i < data.size(); i++) static_cast<char *>(buffer.data())[i] = data[i];
+			parser.commit(data.size());
+		}
+	}
+
+	http_proto::error_code ec;
+	parser.parse(ec);
+	fmt::print("Parse done: '{}', Is complited: '{}', Got header: '{}'.\n", ec.message(), parser.is_complete(), parser.got_header());
+	if(!ec) fmt::print("Buffer: \n{}\n", parser.get().buffer());
+	if(!ec) fmt::print("Body: \n{}\n", parser.body());
+
+	for(auto buffer : parser.prepare())
+	{
+		std::string data = "1231231234243";
+		fmt::print("[write buffer] - data: '{}', size: '{}'.\n", buffer.data(), buffer.size());
+		if(buffer.data() != nullptr && buffer.size() != 0)
+		{
+			for(int i = 0; i < data.size(); i++) static_cast<char *>(buffer.data())[i] = data[i];
+			parser.commit(data.size());
+		}
+	}
+
+	parser.parse(ec);
+	fmt::print("Parse done: '{}', Is complited: '{}', Got header: '{}'.\n", ec.message(), parser.is_complete(), parser.got_header());
+	if(!ec) fmt::print("Buffer: \n{}\n", parser.get().buffer());
+	if(!ec) fmt::print("Body: \n{}\n", parser.body());
+}
+
 int main() try
 {
-	http_proto::response response
-	{
-		"HTTP/1.1 200 OK\r\n"
-		"cache-control: private\r\n"
-		"content-type: text/html; charset=utf-8\r\n"
-		"strict-transport-security: max-age=15552000\r\n"
-		"x-frame-options: SAMEORIGIN\r\n"
-		"content-length: 8\r\n"
-		"\r\n"
-		"somebody"
-	};
-
-	hexdump(response.buffer().data(), response.buffer().size());
+	io::windows::set_asio_locale(io::windows::lang::english);
+	some_tests();
 	return 0;
 }
 catch(const std::exception & e)
