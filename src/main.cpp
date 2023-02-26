f2f660d54a654e367e650f8a94015ac5798ebbcb
Jaz-Wazz <inc.herobrine275@gmail.com>
2023-02-26 10:27:50 +0300

    Simple request parse.

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 #include <iofox.hpp>
 #include <iostream>
 #include <string>
+#include <string_view>
+#include <span>
 #include <vector>
 #include <picohttpparser.h>
 
@@ -13,24 +15,29 @@ namespace beast = boost::beast;			// NOLINT.
 namespace http = beast::http;			// NOLINT.
 namespace this_coro = asio::this_coro;	// NOLINT.
 
+void test_request()
+{
+	const char * method;
+	size_t method_len;
+	const char * path;
+	size_t path_len;
+	int minor_version;
+	struct phr_header headers[4];
+	size_t num_headers;
+
+	const char * buf = "GET /page.sas HTTP/1.1\r\n";
+	int ret = phr_parse_request(buf, 26, &method, &method_len, &path, &path_len, &minor_version, headers, &num_headers, 0);
+	fmt::print("Return: '{}'.\n\n", ret);
+
+	fmt::print("Method: '{}'.\n", std::string(method, method_len));
+	fmt::print("Path: '{}'.\n", std::string(path, path_len));
+	fmt::print("Minor version: '{}'.\n", minor_version);
+	fmt::print("Headers count: '{}'.\n", num_headers);
+}
+
 int main() try
 {
-	// Inputs.
-	char buf[4096];
-	size_t buflen = 4096, prevbuflen = 0;
-
-	// Outputs headers.
-	struct phr_header headers[100] {};
-	size_t num_headers = 100;
-	int minor_version = 0;
-
-	// Outputs.
-	const char *method, *path;
-	size_t method_len = 0, path_len = 0;
-
-	int pret = phr_parse_request(buf, buflen, &method, &method_len, &path, &path_len, &minor_version, headers, &num_headers, prevbuflen);
-	fmt::print("Ret: '{}'.\n", pret);
-
+	test_request();
 	return 0;
 }
 catch(std::exception & e) { fmt::print("Exception: '{}'.\n", e.what()); }
