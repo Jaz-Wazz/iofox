0ed9f51f7a9ce00b291de94b3c70fb1d78257dec
Jaz-Wazz <inc.herobrine275@gmail.com>
2023-02-26 10:55:37 +0300

    Parser class.

--- src/main.cpp
+++ src/main.cpp
@@ -15,36 +15,36 @@ namespace beast = boost::beast;			// NOLINT.
 namespace http = beast::http;			// NOLINT.
 namespace this_coro = asio::this_coro;	// NOLINT.
 
-struct results
+class parser
 {
-	std::string_view method;
-	std::string_view path;
-	int minor_version;
-	std::size_t num_headers;
-	int code;
-};
+	public: std::string_view method;
+	public: std::string_view path;
+	public: int minor_version;
+	public: std::size_t headers_count;
+	public: phr_header headers[4];
 
-results parse(const char * buffer, std::size_t size, phr_header * headers, std::size_t last_len)
-{
-	const char * method, * path;
-	size_t method_len, path_len, num_headers;
-	int minor_version;
-	auto ret = phr_parse_request(buffer, size, &method, &method_len, &path, &path_len, &minor_version, headers, &num_headers, last_len);
-	return {{method, method_len}, {path, path_len}, minor_version, num_headers, ret};
-}
+	public: int parse(const char * buffer, std::size_t size, std::size_t last_len)
+	{
+		const char * method, * path;
+		size_t method_len, path_len;
+		int ret = phr_parse_request(buffer, size, &method, &method_len, &path, &path_len, &minor_version, headers, &headers_count, last_len);
+		this->method = {method, method_len};
+		this->path = {path, path_len};
+		return ret;
+	}
+};
 
 void test_request()
 {
-	struct phr_header headers[4];
-
+	parser parser;
 	const char * buf = "GET /page.sas HTTP/1.1\r\n";
 
-	auto ret = parse(buf, 17, headers, 0);
-	fmt::print("Return: '{}'.\n",			ret.code);
-	fmt::print("Method: '{}'.\n",			ret.method);
-	fmt::print("Path: '{}'.\n",				ret.path);
-	fmt::print("Minor version: '{}'.\n",	ret.minor_version);
-	fmt::print("Headers count: '{}'.\n\n",	ret.num_headers);
+	auto ret = parser.parse(buf, 17, 0);
+	fmt::print("Return: '{}'.\n",			ret);
+	fmt::print("Method: '{}'.\n",			parser.method);
+	fmt::print("Path: '{}'.\n",				parser.path);
+	fmt::print("Minor version: '{}'.\n",	parser.minor_version);
+	fmt::print("Headers count: '{}'.\n\n",	parser.headers_count);
 }
 
 int main() try
