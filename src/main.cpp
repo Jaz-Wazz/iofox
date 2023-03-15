483e7bd7d6c82002e1ac3d6511924b760f74c4a5
Jaz-Wazz <inc.herobrine275@gmail.com>
2023-03-15 03:31:28 +0300

    Recieve messages from clients with static buffer.

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,37 @@
+#include <boost/asio/buffer.hpp>
 #include <boost/asio/co_spawn.hpp>
 #include <boost/asio/io_context.hpp>
+#include <boost/asio/read.hpp>
 #include <boost/asio/this_coro.hpp>
 #include <fmt/core.h>
 #include <iostream>
 #include <string>
 #include <vector>
 #include <iofox/iofox.hpp>
+#include <boost/asio/ip/tcp.hpp>
 
 namespace asio = boost::asio;			// NOLINT.
 namespace this_coro = asio::this_coro;	// NOLINT.
 
+auto session(asio::ip::tcp::socket socket) -> io::coro<void>
+{
+	fmt::print("Connected.\n");
+
+	char buffer[10] {};
+
+	for(;;)
+	{
+		std::size_t readed = co_await socket.async_read_some(asio::mutable_buffer(buffer, 10), io::use_coro);
+		fmt::print("Readed: {} octets: '{}'.\n", readed, std::string(buffer, readed));
+	}
+
+	fmt::print("Disconnected.\n");
+}
+
 auto coro() -> io::coro<void>
 {
-	io::http::start_line start_line {"GET / HTTP/1.0\r\n"};
-	fmt::print("Method: '{}'.\n", start_line.method);
-	fmt::print("Path: '{}'.\n", start_line.path);
-	fmt::print("Version: '{}'.\n", start_line.version);
-	fmt::print("Serialized: '{}'.\n", start_line.serialize());
-	co_return;
+	asio::ip::tcp::acceptor acceptor {co_await this_coro::executor, {asio::ip::tcp::v4(), 80}};
+    for(;;) asio::co_spawn(co_await this_coro::executor, session(co_await acceptor.async_accept(asio::use_awaitable)), io::rethrowed);
 }
 
 int main() try
