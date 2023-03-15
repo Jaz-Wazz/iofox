835bdbce2a30e63df245dcc110a5c6e018c24df9
Jaz-Wazz <inc.herobrine275@gmail.com>
2023-03-15 05:03:13 +0300

    Recieve messages with buffered_stream.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <boost/asio/buffer.hpp>
+#include <boost/asio/buffered_read_stream.hpp>
 #include <boost/asio/co_spawn.hpp>
 #include <boost/asio/completion_condition.hpp>
 #include <boost/asio/io_context.hpp>
@@ -10,6 +11,7 @@
 #include <vector>
 #include <iofox/iofox.hpp>
 #include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/buffered_stream.hpp>
 
 namespace asio = boost::asio;			// NOLINT.
 namespace this_coro = asio::this_coro;	// NOLINT.
@@ -17,11 +19,29 @@ namespace this_coro = asio::this_coro;	// NOLINT.
 auto session(asio::ip::tcp::socket socket) -> io::coro<void>
 {
 	fmt::print("Connected.\n");
+	asio::buffered_read_stream<asio::ip::tcp::socket> buffered_socket {socket.get_executor()};
+	buffered_socket.lowest_layer() = std::move(socket);
 
-	for(std::string buffer;;)
+	for(std::string cmd; std::getline(std::cin, cmd);)
 	{
-		std::size_t readed = co_await asio::async_read(socket, asio::dynamic_buffer(buffer), asio::transfer_at_least(1), io::use_coro);
-		fmt::print("Readed: {} octets: '{}'.\n", readed, buffer);
+		if(cmd == "peak")
+		{
+			std::string peaked_buffer;
+			peaked_buffer.resize(100);
+			std::size_t peaked = buffered_socket.peek(asio::buffer(peaked_buffer));
+			fmt::print("Peak operation: '{}' octets, '{}' data.\n", peaked, std::string(peaked_buffer.c_str(), peaked));
+		}
+		if(cmd == "fill")
+		{
+			std::size_t filled = co_await buffered_socket.async_fill(io::use_coro);
+			fmt::print("Filled operation: '{}' octets.\n", filled);
+		}
+		if(cmd == "read")
+		{
+			std::string buffer;
+			std::size_t readed = co_await asio::async_read(buffered_socket, asio::dynamic_buffer(buffer), asio::transfer_at_least(1), io::use_coro);
+			fmt::print("Readed: {} octets: '{}'.\n", readed, buffer);
+		}
 	}
 
 	fmt::print("Disconnected.\n");
