2da1449f0c68fe6ab52967e93625cb6eabbbdad2
Jaz-Wazz <inc.herobrine275@gmail.com>
2023-03-17 07:46:47 +0300

    Const buffer override experiment.

--- src/main.cpp
+++ src/main.cpp
@@ -21,33 +21,16 @@
 namespace asio = boost::asio;			// NOLINT.
 namespace this_coro = asio::this_coro;	// NOLINT.
 
-auto session(asio::ip::tcp::socket socket) -> io::coro<void>
-{
-	fmt::print("connected.\n");
-	for(std::string cmd; std::getline(std::cin, cmd);)
-	{
-		if(cmd == "read")
-		{
-			std::string buffer_x = std::string(4, '\0');
-			std::string buffer_y = std::string(4, '\0');
-			std::size_t readed = co_await socket.async_read_some(std::array {asio::buffer(buffer_x), asio::buffer(buffer_y)}, io::use_coro);
-			fmt::print("readed: {} octets, buffer_x: '{}', buffer_y: '{}'.\n", readed, buffer_x, buffer_y);
-		}
-	}
-}
-
-auto coro() -> io::coro<void>
-{
-	asio::ip::tcp::acceptor acceptor {co_await this_coro::executor, {asio::ip::tcp::v4(), 80}};
-    for(;;) asio::co_spawn(co_await this_coro::executor, session(co_await acceptor.async_accept(asio::use_awaitable)), io::rethrowed);
-}
-
 int main() try
 {
-	io::windows::set_asio_locale(io::windows::lang::english);
-	asio::io_context ctx;
-	asio::co_spawn(ctx, coro(), io::rethrowed);
-	return ctx.run();
+	const std::string str = "sas";
+	asio::const_buffer const_buffer = asio::buffer(str);
+	fmt::print("ptr: '{}', size: '{}'.\n", const_buffer.data(), const_buffer.size());
+
+	const_buffer = asio::const_buffer(nullptr, 0);
+	fmt::print("ptr: '{}', size: '{}'.\n", const_buffer.data(), const_buffer.size());
+
+	return 0;
 }
 catch(const std::exception & e)
 {
