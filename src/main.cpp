7a30b5270793ef7a16139a7b9808e599ebc1f71f
Jaz-Wazz <inc.herobrine275@gmail.com>
2023-03-17 04:55:19 +0300

    Mutable buffer sequense read experiment.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <array>
 #include <boost/asio/buffer.hpp>
 #include <boost/asio/buffered_read_stream.hpp>
 #include <boost/asio/co_spawn.hpp>
@@ -20,68 +21,17 @@
 namespace asio = boost::asio;			// NOLINT.
 namespace this_coro = asio::this_coro;	// NOLINT.
 
-class my_stream : private asio::ip::tcp::socket
-{
-	public: my_stream(asio::ip::tcp::socket && socket): asio::ip::tcp::socket(std::move(socket)) {}
-	public: auto get_executor() { return asio::ip::tcp::socket::get_executor(); }
-
-	public: using asio::ip::tcp::socket::executor_type;
-
-	public: auto async_read_some(const auto & buffers, auto && token)
-	{
-		return asio::ip::tcp::socket::async_read_some(buffers, std::move(token));
-	}
-};
-
 auto session(asio::ip::tcp::socket socket) -> io::coro<void>
 {
-	my_stream my_stream {std::move(socket)};
-	fmt::print("Connected.\n");
+	fmt::print("connected.\n");
 	for(std::string cmd; std::getline(std::cin, cmd);)
 	{
-		if(cmd == "read_some")
+		if(cmd == "read")
 		{
-			std::string string;
-			string.resize(4);
-			std::size_t readed = co_await my_stream.async_read_some(asio::buffer(string), io::use_coro);
-			fmt::print("Readed: {} octets, message: '{}'.\n", readed, string);
-		}
-		if(cmd == "read_until")
-		{
-			std::string string;
-			// asio::async_read_until()
-			// co_await asio::async_read_until(socket, asio::dynamic_buffer(string), "0", io::use_coro);
-			std::size_t readed = co_await asio::async_read_until(my_stream, asio::dynamic_buffer(string), "0", io::use_coro);
-			fmt::print("Readed: {} octets, message: '{}'.\n", readed, string);
-		}
-		if(cmd == "read_until_custom")
-		{
-			std::string b;
-			auto x = asio::dynamic_buffer(b);
-
-			// [123450fwewef]
-
-			std::string inner_buffer;
-			std::string outer_buffer;
-
-			char buffer[10] {};
-
-			std::size_t readed = co_await my_stream.async_read_some(asio::buffer(buffer), io::use_coro);
-			fmt::print("Readed: {} octets, buffer: '{}'.\n", readed, std::string(buffer, readed));
-
-			for(std::size_t i = 0; i < sizeof(buffer); i++)
-			{
-				if(buffer[i] != '0') outer_buffer.push_back(const char Ch)
-			}
-
-			// for(char c : buffer)
-			// {
-			// 	if(c != '0') outer_buffer.push_back(c);
-			// }
-
-			// std::string str = "12345sas1234";
-			// auto x = str.find_first_of("sas");
-			// fmt::print("Value: '{}'.\n", x);
+			std::string buffer_x = std::string(4, '\0');
+			std::string buffer_y = std::string(4, '\0');
+			std::size_t readed = co_await socket.async_read_some(std::array {asio::buffer(buffer_x), asio::buffer(buffer_y)}, io::use_coro);
+			fmt::print("readed: {} octets, buffer_x: '{}', buffer_y: '{}'.\n", readed, buffer_x, buffer_y);
 		}
 	}
 }
