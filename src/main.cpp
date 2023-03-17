be3e063e9e43b0c4137b2701401342374c86e8a2
Jaz-Wazz <inc.herobrine275@gmail.com>
2023-03-17 04:02:59 +0300

    Dynamic buffer experiment.

--- src/main.cpp
+++ src/main.cpp
@@ -20,84 +20,34 @@
 namespace asio = boost::asio;			// NOLINT.
 namespace this_coro = asio::this_coro;	// NOLINT.
 
-class my_stream : private asio::ip::tcp::socket
-{
-	public: my_stream(asio::ip::tcp::socket && socket): asio::ip::tcp::socket(std::move(socket)) {}
-	public: auto get_executor() { return asio::ip::tcp::socket::get_executor(); }
-
-	public: using asio::ip::tcp::socket::executor_type;
-
-	public: auto async_read_some(const auto & buffers, auto && token)
-	{
-		return asio::ip::tcp::socket::async_read_some(buffers, std::move(token));
-	}
-};
-
-auto session(asio::ip::tcp::socket socket) -> io::coro<void>
-{
-	my_stream my_stream {std::move(socket)};
-	fmt::print("Connected.\n");
-	for(std::string cmd; std::getline(std::cin, cmd);)
-	{
-		if(cmd == "read_some")
-		{
-			std::string string;
-			string.resize(4);
-			std::size_t readed = co_await my_stream.async_read_some(asio::buffer(string), io::use_coro);
-			fmt::print("Readed: {} octets, message: '{}'.\n", readed, string);
-		}
-		if(cmd == "read_until")
-		{
-			std::string string;
-			// asio::async_read_until()
-			// co_await asio::async_read_until(socket, asio::dynamic_buffer(string), "0", io::use_coro);
-			std::size_t readed = co_await asio::async_read_until(my_stream, asio::dynamic_buffer(string), "0", io::use_coro);
-			fmt::print("Readed: {} octets, message: '{}'.\n", readed, string);
-		}
-		if(cmd == "read_until_custom")
-		{
-			std::string b;
-			auto x = asio::dynamic_buffer(b);
-
-			// [123450fwewef]
-
-			std::string inner_buffer;
-			std::string outer_buffer;
-
-			char buffer[10] {};
-
-			std::size_t readed = co_await my_stream.async_read_some(asio::buffer(buffer), io::use_coro);
-			fmt::print("Readed: {} octets, buffer: '{}'.\n", readed, std::string(buffer, readed));
-
-			for(std::size_t i = 0; i < sizeof(buffer); i++)
-			{
-				if(buffer[i] != '0') outer_buffer.push_back(const char Ch)
-			}
-
-			// for(char c : buffer)
-			// {
-			// 	if(c != '0') outer_buffer.push_back(c);
-			// }
-
-			// std::string str = "12345sas1234";
-			// auto x = str.find_first_of("sas");
-			// fmt::print("Value: '{}'.\n", x);
-		}
-	}
-}
-
-auto coro() -> io::coro<void>
-{
-	asio::ip::tcp::acceptor acceptor {co_await this_coro::executor, {asio::ip::tcp::v4(), 80}};
-    for(;;) asio::co_spawn(co_await this_coro::executor, session(co_await acceptor.async_accept(asio::use_awaitable)), io::rethrowed);
-}
-
 int main() try
 {
-	io::windows::set_asio_locale(io::windows::lang::english);
-	asio::io_context ctx;
-	asio::co_spawn(ctx, coro(), io::rethrowed);
-	return ctx.run();
+	std::string string;
+	auto dynamic_buffer = asio::dynamic_buffer(string);
+
+	for(std::string cmd; std::getline(std::cin, cmd);)
+	{
+		if(cmd == "show")
+		{
+			asio::const_buffer buffer = dynamic_buffer.data();
+			fmt::print("Buffer: '{}'.\n", std::string(static_cast<const char *>(buffer.data()), buffer.size()));
+		}
+		if(cmd == "add")
+		{
+			asio::mutable_buffer buffer = dynamic_buffer.prepare(5);
+			std::memcpy(buffer.data(), "garox", 5);
+		}
+		if(cmd == "commit")
+		{
+			dynamic_buffer.commit(1);
+		}
+		if(cmd == "grow")
+		{
+			dynamic_buffer.grow(1);
+		}
+	}
+
+	return 0;
 }
 catch(const std::exception & e)
 {
