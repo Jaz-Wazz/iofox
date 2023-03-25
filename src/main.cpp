6bd5963646b4a119a4ef7003b8cfd5517a243640
Jaz-Wazz <inc.herobrine275@gmail.com>
2023-03-26 02:32:20 +0300

    Experemental socket 3 buffer logging.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include <boost/asio/co_spawn.hpp>
 #include <boost/asio/io_context.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/registered_buffer.hpp>
 #include <boost/asio/this_coro.hpp>
+#include <boost/asio/use_awaitable.hpp>
 #include <fmt/core.h>
 #include <iostream>
+#include <ranges>
 #include <stdexcept>
 #include <string>
 #include <iofox/iofox.hpp>
@@ -10,16 +14,81 @@
 namespace asio = boost::asio;			// NOLINT.
 namespace this_coro = asio::this_coro;	// NOLINT.
 
+auto session(asio::ip::tcp::socket socket) -> io::coro<void>
+{
+	fmt::print("connected.\n");
+	char buffer[8192] {};
+	std::size_t buffer_size = 0;
+	std::size_t prev_buffer_size = 0;
+
+	for(std::string cmd; std::getline(std::cin, cmd);)
+	{
+		if(cmd == "read")
+		{
+			std::size_t readed = co_await socket.async_read_some(asio::buffer(buffer + buffer_size, sizeof(buffer) - buffer_size), io::use_coro);
+
+			auto buffer_0 = asio::buffer(buffer, buffer_size);
+			auto buffer_1 = asio::buffer(buffer + buffer_size, readed);
+			auto buffer_2 = asio::buffer(buffer + buffer_size + readed, sizeof(buffer) - buffer_size - readed);
+			buffer_size += readed;
+
+			fmt::print("┌─────────────────────────────────────────────────────────────────────────────────────────┐\n");
+			fmt::print("│ Buffer dump                                                                             │\n");
+			fmt::print("├───────────┬────────┬─────────────────────────────────────────────────┬──────────────────┤\n");
+
+			for(auto chunk : io::log::hex_dump(buffer_0.data(), buffer_0.size()) | std::views::take(1))
+			{
+				fmt::print("│ Buffer 0: │ {} │ {} │ {} │\n", chunk.offset(), chunk.bytes(), chunk.chars());
+			}
+
+			for(auto chunk : io::log::hex_dump(buffer_0.data(), buffer_0.size()) | std::views::drop(1))
+			{
+				fmt::print("│           │ {} │ {} │ {} │\n", chunk.offset(), chunk.bytes(), chunk.chars());
+			}
+
+			fmt::print("├───────────┼────────┼─────────────────────────────────────────────────┼──────────────────┤\n");
+
+			for(auto chunk : io::log::hex_dump(buffer_1.data(), buffer_1.size()) | std::views::take(1))
+			{
+				fmt::print("│ Buffer 1: │ {} │ {} │ {} │\n", chunk.offset(), chunk.bytes(), chunk.chars());
+			}
+
+			for(auto chunk : io::log::hex_dump(buffer_1.data(), buffer_1.size()) | std::views::drop(1))
+			{
+				fmt::print("│           │ {} │ {} │ {} │\n", chunk.offset(), chunk.bytes(), chunk.chars());
+			}
+
+			fmt::print("├───────────┼────────┼─────────────────────────────────────────────────┼──────────────────┤\n");
+
+			auto dump_2 = io::log::hex_dump(buffer_2.data(), buffer_2.size());
+
+			for(auto chunk : dump_2 | std::views::take(1))
+			{
+				fmt::print("│ Buffer 2: │ {} │ {} │ {} │\n", chunk.offset(), chunk.bytes(), chunk.chars());
+			}
+
+			for(auto chunk : dump_2 | std::views::drop(1) | std::views::take(4))
+			{
+				fmt::print("│           │ {} │ {} │ {} │\n", chunk.offset(), chunk.bytes(), chunk.chars());
+			}
+			fmt::print("│           │        │ {:47} │                  │\n", fmt::format("And {} same lines...", dump_2.size() - 5));
+			fmt::print("└───────────┴────────┴─────────────────────────────────────────────────┴──────────────────┘\n");
+		}
+	}
+}
+
+auto coro() -> io::coro<void>
+{
+	asio::ip::tcp::acceptor acceptor {co_await this_coro::executor, {asio::ip::tcp::v4(), 555}};
+    for(;;) asio::co_spawn(co_await this_coro::executor, session(co_await acceptor.async_accept(asio::use_awaitable)), io::rethrowed);
+}
+
 int main() try
 {
-	std::string str = "r2q3rqh23r8q2ht0q28rht4204\r\nithq230tq92th0q28tq2\r\nhy308rq2hr3h2r2q892\r\n34r98q2yr928q3ryh023r8r2rer";
-
-	for(auto chunk : io::log::hex_dump(str.data(), str.size()))
-	{
-		fmt::print("| {} | {} | {} |\n", chunk.offset(), chunk.bytes(), chunk.chars());
-	}
-
-	return 0;
+	io::windows::set_asio_locale(io::windows::lang::english);
+	asio::io_context ctx;
+	asio::co_spawn(ctx, coro(), io::rethrowed);
+	return ctx.run();
 }
 catch(const std::exception & e)
 {
