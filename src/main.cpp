cad9d13bf67f9f5dc4076ee0d8288779a15ce7b3
Jaz-Wazz <inc.herobrine275@gmail.com>
2023-04-01 00:10:32 +0300

    Review "std::spanstream".

--- src/main.cpp
+++ src/main.cpp
@@ -1,61 +1,38 @@
-#include <iofox/third_party/picohttpparser.h>
-#include <boost/asio/buffer.hpp>
-#include <boost/asio/co_spawn.hpp>
-#include <boost/asio/io_context.hpp>
-#include <boost/asio/ip/tcp.hpp>
-#include <boost/asio/registered_buffer.hpp>
-#include <boost/asio/this_coro.hpp>
-#include <boost/asio/use_awaitable.hpp>
+#include "iofox/iofox.hpp"
+#include <exception>
 #include <fmt/core.h>
+#include <iosfwd>
 #include <iostream>
-#include <ranges>
-#include <stdexcept>
+#include <spanstream>
 #include <string>
-#include <iofox/iofox.hpp>
 
-namespace asio = boost::asio;			// NOLINT.
-namespace this_coro = asio::this_coro;	// NOLINT.
-
-auto session(asio::ip::tcp::socket socket) -> io::coro<void>
+int main() try
 {
-	fmt::print("connected.\n");
-	io::stream stream {std::move(socket)};
+	char buffer[128] {};
+	std::spanstream stream {buffer};
+
+	auto span_buf = stream.rdbuf();
 
 	for(std::string cmd; std::getline(std::cin, cmd);)
 	{
 		if(cmd == "print")
 		{
-			stream.print_buffers();
+			io::log::print_hex_dump(buffer, sizeof(buffer));
+		}
+		if(cmd == "write")
+		{
+			stream << 11 << 22 << 33;
+			stream.flush();
 		}
 		if(cmd == "read")
 		{
-			// [Non buffered read]
-			std::string buffer = std::string(32, '\0');
-			std::size_t readed = co_await stream.async_read_some(asio::buffer(buffer), io::use_coro);
-			fmt::print("readed: {} octets.\n", readed);
-			io::log::print_hex_dump(buffer.data(), buffer.size());
-		}
-		if(cmd == "read_word")
-		{
-			std::string str;
-			co_await (stream >> str);
-			fmt::print("readed: '{}'.\n", str);
+			int i;
+			stream >> i;
+			fmt::print("readed: '{}'.\n", i);
 		}
 	}
-}
 
-auto coro() -> io::coro<void>
-{
-	asio::ip::tcp::acceptor acceptor {co_await this_coro::executor, {asio::ip::tcp::v4(), 555}};
-    for(;;) asio::co_spawn(co_await this_coro::executor, session(co_await acceptor.async_accept(asio::use_awaitable)), io::rethrowed);
-}
-
-int main() try
-{
-	io::windows::set_asio_locale(io::windows::lang::english);
-	asio::io_context ctx;
-	asio::co_spawn(ctx, coro(), io::rethrowed);
-	return ctx.run();
+	return 0;
 }
 catch(const std::exception & e)
 {
