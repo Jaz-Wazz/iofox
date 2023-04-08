fcca6e7a52003c3f83785eb86ac85670d044b9aa
Jaz-Wazz <inc.herobrine275@gmail.com>
2023-04-08 11:03:13 +0300

    Some deferred problems.

--- src/client.cpp
+++ src/client.cpp
@@ -1,11 +1,23 @@
+#include <boost/asio/async_result.hpp>
+#include <boost/asio/awaitable.hpp>
 #include <boost/asio/co_spawn.hpp>
 #include <boost/asio/completion_condition.hpp>
+#include <boost/asio/defer.hpp>
 #include <boost/asio/io_context.hpp>
 #include <boost/asio/ip/address.hpp>
 #include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/steady_timer.hpp>
 #include <boost/asio/this_coro.hpp>
 #include <boost/asio/use_awaitable.hpp>
+#include <boost/asio/deferred.hpp>
+#include <boost/asio/experimental/deferred.hpp>
+#include <boost/asio/experimental/coro_traits.hpp>
+#include <boost/asio/experimental/coro.hpp>
+#include <boost/asio/experimental/awaitable_operators.hpp>
+#include <boost/asio/experimental/co_composed.hpp>
+#include <boost/asio/awaitable.hpp>
 #include <boost/asio/read.hpp>
+#include <boost/asio/use_future.hpp>
 #include <chrono>
 #include <cstring>
 #include <fmt/core.h>
@@ -18,152 +30,58 @@
 #include <span>
 #include <string>
 #include <string_view>
+#include <utility>
+#include <boost/asio.hpp>
 
 namespace asio = boost::asio;			// NOLINT.
 namespace this_coro = asio::this_coro;	// NOLINT.
 
-auto reader_a(asio::ip::tcp::socket socket) -> io::coro<void>
+auto test_a() -> asio::awaitable<void>
 {
-	char * data = new char[528888890];
-	std::size_t size = 0;
+	asio::steady_timer timer {co_await asio::this_coro::executor, std::chrono::seconds(5)};
+	auto deferred_operation = timer.async_wait(asio::deferred);
+	co_await std::move(deferred_operation);
+}
 
-	for(;size != 528888890;)
+auto test_b() -> asio::awaitable<void>
+{
+	asio::steady_timer timer {co_await asio::this_coro::executor, std::chrono::seconds(5)};
+	co_await [&](auto && token)
 	{
-		std::size_t readed = co_await socket.async_read_some(asio::buffer(data + size, 528888890 - size), io::use_coro);
-		size += readed;
-		// fmt::print("[reader 'a'] - readed: {:16} bytes, size: {:16} bytes.\n", readed, size);
-	}
-};
-
-auto reader_b(asio::ip::tcp::socket socket) -> io::coro<void>
-{
-	char * data = new char[528888890];
-	std::size_t readed = co_await asio::async_read(socket, asio::buffer(data, 528888890), io::use_coro);
-	// fmt::print("[reader 'b'] - readed: {} bytes.\n", readed);
-};
-
-auto reader_c(asio::ip::tcp::socket socket) -> io::coro<void>
-{
-	char * data = new char[528888890];
-	std::size_t size = 0;
-
-	for(;size != 528888890;)
-	{
-		auto buffer = asio::buffer(data + size, (528888890 - size > 8192) ? 8192 : 528888890 - size);
-		std::size_t readed = co_await socket.async_read_some(buffer, io::use_coro);
-		size += readed;
-		// fmt::print("[reader 'c'] - readed: {:16} bytes, size: {:16} bytes.\n", readed, size);
-	}
-};
-
-auto reader_d(asio::ip::tcp::socket socket) -> io::coro<void>
-{
-	char * data = new char[528888890];
-	std::size_t size = 0;
-
-	for(;size != 528888890;)
-	{
-		auto buffer = asio::buffer(data + size, (528888890 - size > 128) ? 128 : 528888890 - size);
-		std::size_t readed = co_await socket.async_read_some(buffer, io::use_coro);
-		size += readed;
-		// fmt::print("[reader 'c'] - readed: {:16} bytes, size: {:16} bytes.\n", readed, size);
-	}
-};
-
-auto reader_e(io::isstream stream) -> io::coro<void>
-{
-	char * data = new char[528888890];
-	std::size_t size = 0;
-
-	for(;size != 528888890;)
-	{
-		data[size] = co_await stream.async_get();
-		size++;
-	}
+		return timer.async_wait(io::use_coro);
+	};
 }
 
-auto reader_f(asio::ip::tcp::socket socket) -> io::coro<void>
-{
-	// Object buffer.
-	char * data = new char[528888890];
-	std::size_t size = 0;
+// auto test_c() -> asio::awaitable<void>
+// {
+// 	asio::steady_timer timer {co_await asio::this_coro::executor, std::chrono::seconds(5)};
+// 	auto future = timer.async_wait(asio::use_future);
+// 	co_await future;
+// }
 
-	// Internal buffer.
-	char internal_buffer[4096] {};
-	std::size_t internal_buffer_size = 0;
+// auto test_d() -> asio::awaitable<void>
+// {
+// 	asio::steady_timer timer {co_await asio::this_coro::executor, std::chrono::seconds(5)};
+// 	asio::async_operation<void(boost::system::error_code)> auto my_op = [&](auto && token) {return timer.async_wait(std::move(token));};
+// }
 
-	for(;size != 528888890;)
-	{
-		internal_buffer_size = co_await socket.async_read_some(asio::buffer(internal_buffer), io::use_coro);
-		std::memcpy(data + size, internal_buffer, internal_buffer_size);
-		size += internal_buffer_size;
-		// fmt::print("[reader_f] - read: {:16}, size: {:16}.\n", internal_buffer_size, size);
-	}
+// auto test_x() -> asio::awaitable<void>
+// {
+// 	auto x = asio::deferred.when(true).then(asio::deferred.values('x')).otherwise(asio::deferred.values('y'));
 
-	// std::ofstream("reader_f.txt").write(data, size);
-}
+// 	std::move(x)([](char c){ /* char */ });
+// 	auto c = co_await std::move(x)(io::use_coro);
 
-auto get(char c) -> io::coro<char>
-{
-	co_return c;
-}
+// 	// co_await x;
 
-auto reader_g(asio::ip::tcp::socket socket) -> io::coro<void>
-{
-	// Object buffer.
-	volatile char * data = new char[528888890];
-	std::size_t size = 0;
-
-	// Internal buffer.
-	char internal_buffer[4096] {};
-	std::size_t internal_buffer_size = 0;
-
-	for(;size != 528888890;)
-	{
-		internal_buffer_size = co_await socket.async_read_some(asio::buffer(internal_buffer), io::use_coro);
-
-		for(int i = 0; char c : std::span(internal_buffer, internal_buffer_size))
-		{
-			// data[size + i] = co_await get(c);
-			data[size + i] = c;
-		}
-
-		size += internal_buffer_size;
-		// fmt::print("[reader_f] - read: {:16}, size: {:16}.\n", internal_buffer_size, size);
-	}
-}
-
-auto open_session() -> io::coro<asio::ip::tcp::socket>
-{
-	asio::ip::tcp::socket socket {co_await this_coro::executor};
-	co_await socket.async_connect({asio::ip::make_address("127.0.0.1"), 555}, io::use_coro);
-	co_return socket;
-}
-
-auto benchmark(auto title, auto callable) -> io::coro<void>
-{
-	auto start = std::chrono::steady_clock::now();
-	co_await callable(co_await open_session());
-	auto finish = std::chrono::steady_clock::now();
-	fmt::print("Benchmark '{}': {}.\n", title, std::chrono::duration_cast<std::chrono::milliseconds>(finish - start));
-}
-
-auto coro() -> io::coro<void>
-{
-	// co_await benchmark("reader_a", reader_a);
-	// co_await benchmark("reader_b", reader_b);
-	// co_await benchmark("reader_c", reader_c);
-	// co_await benchmark("reader_d", reader_d);
-	// co_await benchmark("reader_e", reader_e);
-	co_await benchmark("reader_f", reader_f);
-	co_await benchmark("reader_g", reader_g);
-}
+// 	co_return;
+// }
 
 int main() try
 {
 	io::windows::set_asio_locale(io::windows::lang::english);
 	asio::io_context ctx;
-	asio::co_spawn(ctx, coro(), io::rethrowed);
+	// asio::co_spawn(ctx, coro_x(), io::rethrowed);
 	return ctx.run();
 }
 catch(const std::exception & e)
