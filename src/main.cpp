2266881390b20b37a6eff1f5a45ad5f16f48885d
Jaz-Wazz <inc.herobrine275@gmail.com>
2023-09-07 03:04:44 +0300

    Refactor "main.cpp".

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,15 @@
-#include <boost/asio/co_spawn.hpp>
 #include <boost/asio/io_context.hpp>
-#include <boost/asio/this_coro.hpp>
+#include <boost/asio/co_spawn.hpp>
 #include <fmt/core.h>
 #include <iofox.hpp>
-#include <iostream>
-#include <string>
-#include <vector>
 
-namespace asio = boost::asio;			// NOLINT.
-namespace beast = boost::beast;			// NOLINT.
-namespace http = beast::http;			// NOLINT.
-namespace this_coro = asio::this_coro;	// NOLINT.
+namespace asio { using namespace boost::asio; }
 
 auto coro() -> io::coro<void>
 {
-	io::http::request<void> request {"POST", "/post", {{"Host", "httpbin.org"}}};
+	io::http::request request {"POST", "/post", {{"Host", "httpbin.org"}}};
 	auto response = co_await io::http::send("https://httpbin.org", request);
-	std::cout << response << '\n';
+	response.debug_dump();
 }
 
 int main() try
@@ -26,5 +19,7 @@ int main() try
 	asio::co_spawn(ctx, coro(), io::rethrowed);
 	return ctx.run();
 }
-catch(std::exception & e) { fmt::print("Exception: '{}'.\n", e.what()); }
-catch(...) { fmt::print("Exception: 'unknown'.\n"); }
+catch(const std::exception & exception)
+{
+	fmt::print("[main] - exception: '{}'.\n", exception.what());
+}
