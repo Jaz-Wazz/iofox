9af73ba25a66baec59f78c5ead3928930d066eec
Jaz-Wazz <inc.herobrine275@gmail.com>
2024-02-28 03:56:27 +0300

    Integrate "catch2" with some basic tests.

---
+++ src/tests.cpp
@@ -0,0 +1,47 @@
+// Boost.Asio.
+#include <boost/asio/co_spawn.hpp>
+#include <boost/asio/error.hpp>
+#include <boost/asio/io_context.hpp>
+
+// Other.
+#include <catch2/catch_test_macros.hpp>
+#include <boost/beast/http/error.hpp>
+#include <fmt/core.h>
+#include <iofox.hpp>
+
+TEST_CASE("is_common_disconnect")
+{
+	REQUIRE(io::error::is_common_disconnect(boost::asio::error::connection_aborted) == true);
+	REQUIRE(io::error::is_common_disconnect(boost::asio::error::connection_reset) == true);
+	REQUIRE(io::error::is_common_disconnect(boost::asio::error::connection_refused) == true);
+	REQUIRE(io::error::is_common_disconnect(boost::beast::http::error::end_of_stream) == true);
+
+	REQUIRE(io::error::is_common_disconnect(boost::asio::error::not_connected) == false);
+	REQUIRE(io::error::is_common_disconnect(boost::beast::http::error::bad_method) == false);
+}
+
+TEST_CASE("service_mechanic")
+{
+	io::service<int> service;
+
+	auto coro = [&] -> io::coro<void>
+	{
+		int & value = co_await service.get_or_make(0);
+		value++;
+		REQUIRE(value <= 10);
+		co_return;
+	};
+
+	boost::asio::io_context context_x, context_y;
+	for(int i = 0; i < 10; i++) boost::asio::co_spawn(context_x, coro(), io::rethrowed);
+	for(int i = 0; i < 10; i++) boost::asio::co_spawn(context_y, coro(), io::rethrowed);
+	context_x.run();
+	context_y.run();
+}
+
+TEST_CASE("request")
+{
+	io::http::request<std::string> request;
+	request.body() = "some_string";
+	REQUIRE(typeid(request.body()) == typeid(std::string));
+}
