2e13e0dcccee100e1b78c21c1e3de22141276cdf
Jaz-Wazz <inc.herobrine275@gmail.com>
2024-02-28 05:16:27 +0300

    Temporary removed "request_header" class.

--- src/iofox.hpp
+++ src/iofox.hpp
@@ -337,25 +337,6 @@ namespace io::http
 		}
 	};
 
-	// Basic request header object.
-	class request_header: public beast::http::request_header<>
-	{
-		prv using base = beast::http::request_header<>;
-		prv using header_list = std::initializer_list<std::pair<std::string, std::string>>;
-
-		pbl using base::operator=;
-		pbl using base::operator[];
-
-		pbl request_header(std::string method = "GET", std::string target = "/", header_list headers = {})
-		{
-			this->method_string(method);
-			this->target(target);
-			for(auto && [header, value] : headers) this->insert(header, value);
-		}
-
-		pbl request_header(beast::http::request_header<> && header): base(std::move(header)) {}
-	};
-
 	// Basic response header object.
 	class response_header: public beast::http::response_header<>
 	{
