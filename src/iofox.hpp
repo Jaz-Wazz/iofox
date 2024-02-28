dd70f5e867566b6ab82b4df39e346f59c7dc2970
Jaz-Wazz <inc.herobrine275@gmail.com>
2024-02-28 05:17:52 +0300

    Temporary removed "response_header" class.

--- src/iofox.hpp
+++ src/iofox.hpp
@@ -336,24 +336,6 @@ namespace io::http
 			fmt::print("[response] - {}\n", (std::stringstream() << *this).str());
 		}
 	};
-
-	// Basic response header object.
-	class response_header: public beast::http::response_header<>
-	{
-		prv using base = beast::http::response_header<>;
-		prv using header_list = std::initializer_list<std::pair<std::string, std::string>>;
-
-		pbl using base::operator=;
-		pbl using base::operator[];
-
-		pbl response_header(unsigned int result = 200, header_list headers = {})
-		{
-			this->result(result);
-			for(auto && [header, value] : headers) this->insert(header, value);
-		}
-
-		pbl response_header(beast::http::response_header<> && header): base(std::move(header)) {}
-	};
 }
 
 namespace io::http
