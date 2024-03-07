4018b6cc861f817f199b6d9b7223b988c4971ad1
Jaz-Wazz <inc.herobrine275@gmail.com>
2024-03-07 03:33:53 +0300

    Extract test "http.cpp".

--- test/tests.cpp
+++ test/tests.cpp
@@ -29,60 +29,6 @@
 #include <catch2/catch_test_macros.hpp>
 #include <fmt/core.h>
 
-TEST_CASE("request")
-{
-	SECTION("void")
-	{
-		iofox::http::request request;
-		REQUIRE(typeid(request.body()) == typeid(boost::beast::http::empty_body::value_type));
-	}
-
-	SECTION("std::string")
-	{
-		iofox::http::request<std::string> request;
-		REQUIRE(typeid(request.body()) == typeid(std::string));
-	}
-
-	SECTION("std::vector<char>")
-	{
-		iofox::http::request<std::vector<char>> request;
-		REQUIRE(typeid(request.body()) == typeid(std::vector<char>));
-	}
-
-	SECTION("std::vector<std::int8_t>")
-	{
-		iofox::http::request<std::vector<std::int8_t>> request;
-		REQUIRE(typeid(request.body()) == typeid(std::vector<std::int8_t>));
-	}
-}
-
-TEST_CASE("response")
-{
-	SECTION("void")
-	{
-		iofox::http::response response;
-		REQUIRE(typeid(response.body()) == typeid(boost::beast::http::empty_body::value_type));
-	}
-
-	SECTION("std::string")
-	{
-		iofox::http::response<std::string> response;
-		REQUIRE(typeid(response.body()) == typeid(std::string));
-	}
-
-	SECTION("std::vector<char>")
-	{
-		iofox::http::response<std::vector<char>> response;
-		REQUIRE(typeid(response.body()) == typeid(std::vector<char>));
-	}
-
-	SECTION("std::vector<std::int8_t>")
-	{
-		iofox::http::response<std::vector<std::int8_t>> response;
-		REQUIRE(typeid(response.body()) == typeid(std::vector<std::int8_t>));
-	}
-}
-
 TEST_CASE("send")
 {
 	auto coro_x = [&] -> iofox::coro<void>
