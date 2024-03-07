416d66a28e7304334c65fd1c439d4e699e630edb
Jaz-Wazz <inc.herobrine275@gmail.com>
2024-03-07 03:25:35 +0300

    Extract test "error.cpp".

--- test/tests.cpp
+++ test/tests.cpp
@@ -29,27 +29,6 @@
 #include <catch2/catch_test_macros.hpp>
 #include <fmt/core.h>
 
-TEST_CASE("is_common_disconnect")
-{
-	REQUIRE(iofox::error::is_common_disconnect(boost::asio::error::connection_aborted) == true);
-	REQUIRE(iofox::error::is_common_disconnect(boost::asio::error::connection_reset) == true);
-	REQUIRE(iofox::error::is_common_disconnect(boost::asio::error::connection_refused) == true);
-	REQUIRE(iofox::error::is_common_disconnect(boost::beast::http::error::end_of_stream) == true);
-
-	REQUIRE(iofox::error::is_common_disconnect(boost::asio::error::not_connected) == false);
-	REQUIRE(iofox::error::is_common_disconnect(boost::beast::http::error::bad_method) == false);
-}
-
-TEST_CASE("is_common_timeout")
-{
-	REQUIRE(iofox::error::is_common_timeout(boost::asio::error::timed_out) == true);
-	REQUIRE(iofox::error::is_common_timeout(boost::beast::error::timeout) == true);
-	REQUIRE(iofox::error::is_common_timeout(boost::system::error_code(121, boost::asio::error::system_category)) == true);
-
-	REQUIRE(iofox::error::is_common_timeout(boost::asio::error::not_connected) == false);
-	REQUIRE(iofox::error::is_common_timeout(boost::beast::http::error::bad_method) == false);
-}
-
 TEST_CASE("service_mechanic")
 {
 	iofox::service<int> service;
