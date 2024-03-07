4024379e6be106b1b2e8221261b6fe63e942069c
Jaz-Wazz <inc.herobrine275@gmail.com>
2024-03-07 03:45:42 +0300

    Extract test "this_thread.cpp".

--- test/this_thread.cpp
+++ test/this_thread.cpp
@@ -1,33 +1,15 @@
 // boost_asio
-#include <boost/asio/co_spawn.hpp>
-#include <boost/asio/error.hpp>
 #include <boost/asio/io_context.hpp>
 #include <boost/asio/ip/tcp.hpp>
-
-// boost_beast
-#include <boost/beast/http/empty_body.hpp>
-#include <boost/beast/http/error.hpp>
 
 // boost_system
 #include <boost/system/error_code.hpp>
 
-// stl
-#include <cstdint>
-#include <string>
-#include <vector>
-
 // iofox
-#include <iofox/coro.hpp>
-#include <iofox/error.hpp>
-#include <iofox/service.hpp>
-#include <iofox/rethrowed.hpp>
-#include <iofox/http.hpp>
-#include <iofox/send.hpp>
 #include <iofox/this_thread.hpp>
 
-// other
+// catch2
 #include <catch2/catch_test_macros.hpp>
-#include <fmt/core.h>
 
 TEST_CASE("this_thread_language")
 {
