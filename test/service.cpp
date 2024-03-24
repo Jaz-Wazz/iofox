0cdafb835731d2f8a00b7ce3d1c92b5450b382bd
Jaz-Wazz <inc.herobrine275@gmail.com>
2024-03-24 08:16:54 +0300

    Nepon.

--- test/service.cpp
+++ test/service.cpp
@@ -6,6 +6,7 @@
 #include <boost/asio/io_context.hpp>
 #include <boost/asio/co_spawn.hpp>
 #include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/is_executor.hpp>
 #include <boost/asio/ssl/context.hpp>
 #include <boost/asio/this_coro.hpp>
 #include <boost/asio/strand.hpp>
@@ -37,6 +38,37 @@ struct executor_wrapper: public boost::asio::io_context::executor_type
 	// public: using boost::asio::io_context::basic_executor_type<std::allocator<void>, 0>::basic_executor_type;
 };
 
+struct custom_property_t
+{
+    int custom_value = 10;
+
+	template <typename T>
+	static constexpr bool is_applicable_property_v = boost::asio::is_executor<T>::value;
+
+	static constexpr bool is_requirable = false;
+	static constexpr bool is_preferable = false;
+
+	typedef std::any polymorphic_query_result_type;
+
+	constexpr custom_property_t() {}
+
+	// template <typename Executor>
+	// static constexpr decltype(auto) static_query_v = Executor::query(custom_property_t());
+};
+
+constexpr custom_property_t custom_property;
+
+// template <typename Executor>
+// custom_property boost::asio::query(Executor& executor, custom_property)
+// {
+//     // Implement the logic to obtain the custom property value from the executor
+//     // This could involve accessing executor-specific information or state
+//     custom_property result;
+
+//     // Populate result with the custom property value
+//     return result;
+// }
+
 TEST_CASE("one")
 {
 	boost::asio::execution::context_t x;
@@ -46,6 +78,7 @@ TEST_CASE("one")
 	auto & result_x = boost::asio::query(io_context.get_executor(), boost::asio::execution::context);
 	auto & result_y = boost::asio::query(executor_wrapper, boost::asio::execution::context);
 	auto result_z = boost::asio::query(executor_wrapper, boost::asio::execution::allocator);
+	auto result_g = boost::asio::query(executor_wrapper, custom_property);
 
 	// io_context.get_executor().
 	// wrapper.
