a16e65ebb88d0cb5fe9198642458691961ccac84
Jaz-Wazz <inc.herobrine275@gmail.com>
2024-03-24 10:02:42 +0300

    Basic work implementation of concept.

--- test/service.cpp
+++ test/service.cpp
@@ -1,4 +1,5 @@
 // boost_asio
+#include <boost/asio/any_io_executor.hpp>
 #include <boost/asio/bind_executor.hpp>
 #include <boost/asio/execution/allocator.hpp>
 #include <boost/asio/execution/context.hpp>
@@ -21,28 +22,43 @@
 
 // catch2
 #include <catch2/catch_test_macros.hpp>
+#include <stdexcept>
 
-struct executor_wrapper: public boost::asio::io_context::executor_type
+namespace iofox
 {
-	boost::asio::ssl::context * ssl_context = nullptr;
-};
+	struct executor_wrapper: public boost::asio::io_context::executor_type
+	{
+		boost::asio::ssl::context * ssl_context = nullptr;
+	};
+
+	auto get_ssl_context(const boost::asio::any_io_executor & any_executor) -> boost::asio::ssl::context &
+	{
+		auto * ptr = any_executor.target<executor_wrapper>();
+		if(ptr != nullptr) return *(ptr->ssl_context); else throw std::runtime_error("executor_not_contain_ssl_context");
+	}
+
+	auto make_executor_wrapper(boost::asio::io_context & io_context, boost::asio::ssl::context & ssl_context)
+	{
+		return executor_wrapper {io_context.get_executor(), &ssl_context};
+	}
+}
 
 TEST_CASE("one")
 {
 	boost::asio::io_context io_context;
-	executor_wrapper executor_wrapper {io_context.get_executor()};
+	boost::asio::ssl::context ssl_context_server {boost::asio::ssl::context::tls};
+	boost::asio::ssl::context ssl_context_client {boost::asio::ssl::context::tls};
+	auto executor_server = iofox::make_executor_wrapper(io_context, ssl_context_server);
+	auto executor_client = iofox::make_executor_wrapper(io_context, ssl_context_client);
 
-	boost::asio::ssl::context ssl_context {boost::asio::ssl::context::tls};
-	executor_wrapper.ssl_context = &ssl_context;
-
-	auto coro = [] -> iofox::coro<void>
+	auto coro = [](const char * name) -> iofox::coro<void>
 	{
-		const auto executor = co_await boost::asio::this_coro::executor;
-		auto ex = executor.target<class executor_wrapper>();
-		fmt::print("[coro] - my ssl context is: '{}'.\n", (void *) ex->ssl_context);
+		auto & ssl_context = iofox::get_ssl_context(co_await boost::asio::this_coro::executor);
+		fmt::print("[coro '{}'] - my ssl context is: '{}'.\n", name, (void *) &ssl_context);
 		co_return;
 	};
 
-	boost::asio::co_spawn(executor_wrapper, coro(), iofox::rethrowed);
+	boost::asio::co_spawn(executor_server, coro("server"), iofox::rethrowed);
+	boost::asio::co_spawn(executor_client, coro("client"), iofox::rethrowed);
 	io_context.run();
 }
