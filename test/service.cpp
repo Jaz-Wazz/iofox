14b4304c36772bdf4af486830c61825847d1cb2b
Jaz-Wazz <inc.herobrine275@gmail.com>
2024-03-28 02:46:06 +0300

    Research "async_initiate" mechanism.

--- test/service.cpp
+++ test/service.cpp
@@ -5,6 +5,7 @@
 #include <boost/asio/async_result.hpp>
 #include <boost/asio/bind_executor.hpp>
 #include <boost/asio/buffer.hpp>
+#include <boost/asio/error.hpp>
 #include <boost/asio/experimental/cancellation_condition.hpp>
 #include <boost/asio/io_context.hpp>
 #include <boost/asio/co_spawn.hpp>
@@ -15,6 +16,7 @@
 #include <boost/asio/steady_timer.hpp>
 #include <boost/asio/this_coro.hpp>
 #include <boost/asio/use_awaitable.hpp>
+#include <boost/system/detail/error_code.hpp>
 #include <chrono>
 #include <functional>
 #include <initializer_list>
@@ -34,72 +36,83 @@
 #include <boost/asio/experimental/parallel_group.hpp>
 using namespace std::chrono_literals;
 
-template <class CompletionToken>
-struct timed_token
-{
-	std::chrono::milliseconds timeout;
-	CompletionToken& token;
-};
+// template <class CompletionToken>
+// struct timed_token
+// {
+// 	CompletionToken& token;
+// };
 
-template <class... Signatures>
-struct timed_initiation
-{
-	template <class CompletionHandler, class Initiation, class... InitArgs>
-	void operator()(CompletionHandler handler, std::chrono::milliseconds timeout, Initiation&& initiation, InitArgs&&... init_args)
-	{
-		using boost::asio::experimental::make_parallel_group;
-		using boost::asio::experimental::wait_for_one;
+// template <class... Signatures>
+// struct timed_initiation
+// {
+// 	template <class CompletionHandler, class Initiation, class... InitArgs>
+// 	void operator()(CompletionHandler handler, Initiation&& initiation, InitArgs&&... init_args)
+// 	{
+// 		using boost::asio::experimental::make_parallel_group;
+// 		using boost::asio::experimental::wait_for_one;
 
-		auto ex		= boost::asio::get_associated_executor(handler, boost::asio::get_associated_executor(initiation));
-		auto alloc	= boost::asio::get_associated_allocator(handler);
-		auto timer	= std::allocate_shared<boost::asio::steady_timer>(alloc, ex, timeout);
+// 		auto ex		= boost::asio::get_associated_executor(handler, boost::asio::get_associated_executor(initiation));
+// 		auto alloc	= boost::asio::get_associated_allocator(handler);
 
-		auto op_timeout = boost::asio::bind_executor(ex, [&](auto && token)
-		{
-			return timer->async_wait(std::forward<decltype(token)>(token));
-		});
+// 		auto op_underlying = boost::asio::bind_executor(ex, [&](auto && token)
+// 		{
+// 			return boost::asio::async_initiate<decltype(token), Signatures...>
+// 			(
+// 				std::forward<Initiation>(initiation), token,
+// 				std::forward<InitArgs>(init_args)...
+// 			);
+// 		});
 
-		auto op_underlying = boost::asio::bind_executor(ex, [&](auto && token)
-		{
-			return boost::asio::async_initiate<decltype(token), Signatures...>
-			(
-				std::forward<Initiation>(initiation), token,
-				std::forward<InitArgs>(init_args)...
-			);
-		});
+// 		auto group = make_parallel_group(op_underlying);
+// 		group.async_wait(wait_for_one(), [handler = std::move(handler)](std::array<std::size_t, 1>, std::error_code, auto... underlying_op_results) mutable
+// 		{
+// 			std::move(handler)(std::move(underlying_op_results)...);
+// 		});
+// 	}
+// };
 
-		auto group = make_parallel_group(op_timeout, op_underlying);
-		group.async_wait(wait_for_one(), [handler = std::move(handler), timer](std::array<std::size_t, 2>, std::error_code, auto... underlying_op_results) mutable
-		{
-			timer.reset();
-			std::move(handler)(std::move(underlying_op_results)...);
-		});
-	}
-};
+// template <class InnerCompletionToken, class... Signatures>
+// struct boost::asio::async_result<timed_token<InnerCompletionToken>, Signatures...>
+// {
+// 	template <class Initiation, class... InitArgs>
+// 	static auto initiate(Initiation && init, timed_token<InnerCompletionToken> t, InitArgs &&... init_args)
+// 	{
+// 		return asio::async_initiate<InnerCompletionToken, Signatures...>
+// 		(
+// 			timed_initiation<Signatures...>{},
+// 			t.token,
+// 			std::forward<Initiation>(init),
+// 			std::forward<InitArgs>(init_args)...
+// 		);
+// 	}
+// };
 
-template <class InnerCompletionToken, class... Signatures>
-struct boost::asio::async_result<timed_token<InnerCompletionToken>, Signatures...>
-{
-	template <class Initiation, class... InitArgs>
-	static auto initiate(Initiation && init, timed_token<InnerCompletionToken> t, InitArgs &&... init_args)
-	{
-		return asio::async_initiate<InnerCompletionToken, Signatures...>
-		(
-			timed_initiation<Signatures...>{},
-			t.token,
-			t.timeout,
-			std::forward<Initiation>(init),
-			std::forward<InitArgs>(init_args)...
-		);
-	}
-};
+#include <boost/core/demangle.hpp>
 
 auto coro() -> iofox::coro<void>
 {
-	boost::asio::steady_timer timer {co_await boost::asio::this_coro::executor, 5s};
 	fmt::print("run.\n");
-	auto [ec] = co_await timer.async_wait(boost::asio::as_tuple(timed_token{10000ms, boost::asio::use_awaitable}));
-	fmt::print("result: '{}'.\n", ec.message());
+	boost::asio::steady_timer timer {co_await boost::asio::this_coro::executor, 5s};
+
+	boost::asio::use_awaitable_t token;
+	int result = co_await boost::asio::async_initiate<boost::asio::use_awaitable_t<>, void(boost::system::error_code, int)>([&](auto && handler)
+	{
+		auto executor = boost::asio::get_associated_executor(handler);
+		fmt::print("handler type: '{}'.\n", boost::core::demangle(typeid(handler).name()));
+		fmt::print("executor type: '{}'.\n", boost::core::demangle(typeid(executor).name()));
+		fmt::print("executor wrapped type: '{}'.\n", boost::core::demangle(executor.target_type().name()));
+
+		// handler(boost::asio::error::name_too_long, 10);
+		// handler({}, 10);
+
+		timer.async_wait([handler = std::move(handler)](auto ec) mutable
+		{
+			fmt::print("handler inner type: '{}'.\n", boost::core::demangle(typeid(handler).name()));
+			handler({}, 15);
+		});
+	}, token);
+
+	fmt::print("end, result: '{}'.\n", result);
 }
 
 TEST_CASE()
