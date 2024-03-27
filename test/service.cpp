f4d3ef959ca0809701df08c23cd2639a0ac473c0
Jaz-Wazz <inc.herobrine275@gmail.com>
2024-03-27 17:03:26 +0300

    Hmmm.

--- test/service.cpp
+++ test/service.cpp
@@ -1,5 +1,6 @@
 // boost_asio
 #include <boost/asio/any_io_executor.hpp>
+#include <boost/asio/as_tuple.hpp>
 #include <boost/asio/buffer.hpp>
 #include <boost/asio/io_context.hpp>
 #include <boost/asio/co_spawn.hpp>
@@ -7,6 +8,9 @@
 #include <boost/asio/ssl/context.hpp>
 
 // iofox
+#include <boost/asio/steady_timer.hpp>
+#include <boost/asio/this_coro.hpp>
+#include <boost/asio/use_awaitable.hpp>
 #include <chrono>
 #include <functional>
 #include <initializer_list>
@@ -22,93 +26,8 @@
 // catch2
 #include <catch2/catch_test_macros.hpp>
 
-namespace iofox
-{
-	struct executor_wrapper: public boost::asio::io_context::executor_type
-	{
-		boost::asio::ssl::context * ssl_context = nullptr;
-	};
-
-	auto get_ssl_context(const boost::asio::any_io_executor & any_executor) -> boost::asio::ssl::context &
-	{
-		auto * ptr = any_executor.target<executor_wrapper>();
-		if(ptr != nullptr) return *(ptr->ssl_context); else throw std::runtime_error("executor_not_contain_ssl_context");
-	}
-
-	auto make_executor_wrapper(boost::asio::io_context & io_context, boost::asio::ssl::context & ssl_context)
-	{
-		return executor_wrapper {io_context.get_executor(), &ssl_context};
-	}
-}
-
-TEST_CASE("one")
-{
-	boost::asio::io_context io_context;
-	boost::asio::ssl::context ssl_context_server {boost::asio::ssl::context::tls};
-	boost::asio::ssl::context ssl_context_client {boost::asio::ssl::context::tls};
-	auto executor_server = iofox::make_executor_wrapper(io_context, ssl_context_server);
-	auto executor_client = iofox::make_executor_wrapper(io_context, ssl_context_client);
-
-	auto coro = [](const char * name) -> iofox::coro<void>
-	{
-		auto & ssl_context = iofox::get_ssl_context(co_await boost::asio::this_coro::executor);
-		fmt::print("[coro '{}'] - my ssl context is: '{}'.\n", name, (void *) &ssl_context);
-		co_return;
-
-		// auto & ssl_context	= iofox::get_ssl_context(executor);
-		// auto & dns_resolver	= iofox::get_dns_resolver(executor);
-		// auto & timeout		= iofox::get_timeout(executor);
-		// auto & retry_handler	= iofox::get_retry_handler(executor);
-
-		// co_await some_http_operation(); <- ssl_context, dns_resolver, timeout, retry_handler
-
-		// auto executor = co_await iofox::this_coro::executor;
-
-
-		// Structured concurency
-		// iofox::retry(iofox::http::send(args...), timeout_handler());
-
-		// Structured concurency
-		// iofox::http::send(args...) | iofox::retry(timeout_handler());
-	};
-
-	boost::asio::co_spawn(executor_server, coro("server"), iofox::rethrowed);
-	boost::asio::co_spawn(executor_client, coro("client"), iofox::rethrowed);
-	io_context.run();
-}
-
-// auto respone = http_send("google.com", request, 15s);
-
-struct op_teta
-{
-	iofox::coro<int> retry(auto handler)
-	{
-		return {};
-	}
-};
-
-struct op_beta
-{
-	op_teta * timeout(int time)
-	{
-		return {};
-	}
-};
-
-struct operation_socket_read
-{
-	op_beta * operator ->()
-	{
-		return {};
-	}
-};
-
-operation_socket_read socket_read()
-{
-	return {};
-}
-
 #include <boost/asio/deferred.hpp>
+using namespace std::chrono_literals;
 
 namespace iofox
 {
@@ -144,84 +63,53 @@ namespace iofox
 	{
 		co_return 0;
 	}
+
+	template <typename... T>
+	inline auto repeat(int count, boost::asio::deferred_async_operation<T...> operation) -> iofox::coro<void>
+	{
+		boost::asio::deferred_async_operation<T...> op_x = operation;
+		boost::asio::deferred_async_operation<T...> op_y = operation;
+		boost::asio::deferred_async_operation<T...> op_z = operation;
+		fmt::print(".\n");
+
+		// op_x | op_y;
+
+		// boost::asio::deferred | boost::asio::use_awaitable;
+
+
+		auto use_tuble_and_awaitable = boost::asio::as_tuple(boost::asio::use_awaitable);
+		// iofox::use_tuple | iofox::use_awaitable
+		// iofox::repeat(10, socket.async_write_some(args..., token));
+		// auto result = co_await socket.async_write_some(args..., iofox::use_repeat(10, token));
+		// auto result = co_await socket.async_write_some(args..., iofox::use_timeout(10s, iofox::use_repeat(10, token)));
+		// auto result = co_await socket.async_write_some(args..., token | iofox::use_timeout(10s) | iofox::use_repeat(10));
+		// auto result = co_await (socket.async_write_some(args..., token) | iofox::use_timeout(10s) | iofox::use_repeat(10)));
+
+		co_await std::move(op_x);
+		fmt::print(".\n");
+		co_await std::move(op_y);
+		fmt::print(".\n");
+		co_await std::move(op_z);
+		fmt::print(".\n");
+	}
 }
 
-auto foo() -> iofox::coro<void>
+auto coro() -> iofox::coro<void>
 {
-	// auto retry_handler = [](){};
-	// auto result = co_await socket_read() -> timeout(10000) -> retry(retry_handler);
+	boost::asio::steady_timer timer {co_await boost::asio::this_coro::executor, 1s};
 
-	// auto request = ...;
-	// auto response = co_await iofox::send("https://google.com", request) -> timeout(10s) -> retry(retry_handler);
+	// fmt::print("wait 1 sec.\n");
+	// co_await timer.async_wait(iofox::use_coro);
+	// fmt::print("wait end.\n");
 
-	// auto result = co_await socket.async_write_some(boost::asio::buffer(array, array_size), iofox::token)
-	//						  -> timeout(10s)
-	//						  -> retry(retry_handler);
+	fmt::print("wait 1 sec.\n");
+	co_await iofox::repeat(10, timer.async_wait(boost::asio::deferred));
+	fmt::print("wait end.\n");
+}
 
-	// auto result = co_await (socket.async_write_some(boost::asio::buffer(array, array_size), iofox::token)
-	//						  | timeout(10s)
-	//						  | retry(retry_handler));
-
-	// auto result = co_await iofox::async_operation
-	// {
-	// 	socket.async_write_some(boost::asio::buffer(array, array_size), iofox::deffered),
-	// 	iofox::timeout(10s),
-	// 	iofox::retry(retry_handler)
-	// };
-
-	// auto result = iofox::retry(retry_handler, iofox::timeout(10s,
-	// {
-	// 	socket.async_write_some(boost::asio::buffer(array, array_size), iofox::deffered)
-	// }));
-
-	// auto result = iofox::retry_timeout(
-	// {
-	// 	socket.async_write_some(boost::asio::buffer(array, array_size), iofox::deffered)
-	// }, 10s, retry_handler);
-
-	// operation_alpha operation_alpha;
-	// operation_alpha->
-
+TEST_CASE()
+{
 	boost::asio::io_context io_context;
-	boost::asio::ip::tcp::socket socket {io_context};
-
-	using namespace std::chrono_literals;
-
-	// auto def = socket.async_write_some(boost::asio::buffer("sas"), boost::asio::deferred);
-
-	// auto result = co_await iofox::timeout(15s, socket.async_write_some(boost::asio::buffer("sas"), boost::asio::deferred));
-
-	// auto result = co_await iofox::timeout(15s,
-	// {
-	// 	socket.async_write_some(boost::asio::buffer("sas"), boost::asio::deferred)
-	// });
-
-	// auto handler = [](){};
-	// auto result = co_await iofox::retry(handler, socket.async_write_some(boost::asio::buffer("sas"), boost::asio::deferred));
-
-	// auto handler = [](){};
-	// auto result = co_await iofox::retry(handler,
-	// {
-	// 	socket.async_write_some(boost::asio::buffer("sas"), boost::asio::deferred)
-	// });
-
-	// auto handler = [](){};
-	// auto result = co_await iofox::retry_timeout(handler, 15s, socket.async_write_some(boost::asio::buffer("sas"), boost::asio::deferred));
-
-	auto handler = [](){};
-	auto result = co_await iofox::retry_timeout(handler, 15s,
-	{
-		socket.async_write_some(boost::asio::buffer("sas"), boost::asio::deferred)
-	}); // <-- token &&&&&&
-
-	// foox(socket.async_write_some(boost::asio::buffer("sas"), boost::asio::deferred));
-
-	// auto handler = [](){};
-	// auto op = socket.async_write_some(boost::asio::buffer("sas"), boost::asio::deferred);
-	// auto result = co_await iofox::retry_timeout(handler, 15s, op);
-
-	// auto result = co_await socket.async_write_some(boost::asio::buffer("sas"), iofox::co_rat(handler, 15s));
-
-	co_return;
-	// std::function<int()> func_ret_int = []{ co_return 5; };
+	boost::asio::co_spawn(io_context, coro(), iofox::rethrowed);
+	io_context.run();
 }
