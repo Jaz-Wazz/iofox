2855842bf60a130a8b257499885f3fb88c9a098b
Jaz-Wazz <inc.herobrine275@gmail.com>
2025-02-25 03:10:23 +0300

    Split `boost` mono-patch to independent patches.

--- packages/boost.cmake
+++ packages/boost.cmake
@@ -5,8 +5,12 @@ CPMAddPackage(
 	NAME "Boost"
 	VERSION "1.87.0"
 	URL "https://github.com/boostorg/boost/releases/download/boost-1.87.0/boost-1.87.0-cmake.tar.xz"
-	PATCHES "../patches/boost.patch"
 	CUSTOM_CACHE_KEY "263a30dc8c4d78dc7c709175eea1838c88c766b9"
+	PATCHES
+		"../patches/0000-boost-url-cmake-min-version.patch"
+		"../patches/0001-boost-filesystem-cmake-min-version.patch"
+		"../patches/0002-boost-asio-wolfssl-error-propogation.patch"
+		"../patches/0003-boost-asio-wolfssl-error-message.patch"
 	OPTIONS
 		"BOOST_ENABLE_CMAKE ON"
 		"BOOST_SKIP_INSTALL_RULES ON"
